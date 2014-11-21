7910d88f145370c71a1a66383494ca0db66cc00a
Jan Ivar Beddari <code@beddari.net>
2014-11-21 18:43:03 +0100

    Basic skeleton

---
+++ Puppetfile
@@ -0,0 +1,9 @@
+
+mod 'puppetlabs/ntp',
+  :git => 'https://github.com/puppetlabs/puppetlabs-ntp',
+  :ref => '3.3.0'
+
+mod 'puppetlabs/stdlib',
+  :git => 'https://github.com/puppetlabs/puppetlabs-stdlib',
+  :ref => '4.4.0'
+
---
+++ site/site.pp
@@ -0,0 +1,18 @@
+# Assuming trusted_node_data = true in puppet.conf
+# https://docs.puppetlabs.com/puppet/3/reference/lang_variables.html#trusted-node-data
+# 
+# We expect a certain certname format, e.g
+#  * role-location-identifier.domain.com
+#  * haproxy-bgo-01.snakeoil.int
+#
+# Parse data from $trusted['certname'] for hiera lookup
+$verified_certname = $trusted['certname']
+$certname_a        = split($verified_certname, "-")
+$role              = $::certname_a[0]
+$location          = $::certname_a[1]
+
+hiera_include('classes', [])
+
+# Empty default node
+node default { }
+
