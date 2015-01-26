852323510019766e79b27d18ba27d8d9d5660768
Jan Ivar Beddari <code@beddari.net>
2015-01-26 11:51:54 +0100

    Updated libvirt install scripts

--- provision/install_libvirt.sh
+++ provision/install_libvirt.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # CentOS 7, Fedora 20/21
-sudo yum -y install qemu-kvm libvirt virt-install bridge-utils virt-install
+sudo yum -y install qemu-kvm libvirt virt-install bridge-utils libvirt-daemon-kvm
 sudo systemctl start libvirtd
 sudo systemctl enable libvirtd
