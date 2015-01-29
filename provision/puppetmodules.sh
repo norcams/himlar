4a81c554b2bccfccc17e5f79e6912f7caf393a09
Jan Ivar Beddari <code@beddari.net>
2015-01-29 15:43:14 +0100

    Cleanup of puppetmodules.sh
    Simplified and added documentation

--- provision/puppetmodules.sh
+++ provision/puppetmodules.sh
@@ -21,17 +21,15 @@ etc_puppet_modules="$(ls -d /etc/puppet/modules/*/ 2>/dev/null)"
 opt_himlar_modules="$(ls -d /opt/himlar/modules/*/ 2>/dev/null)"
 
 # Default value is to NOT provision from Puppetfile
+# We provison Puppetfile if /etc/puppet/modules is empty, if HIMLAR_PUPPETFILE
+# is set to 'deploy' or if puppetmodules.sh is given the 'deploy' parameter
 puppetfile=false
-# We provison Puppetfile if /etc/puppet/modules is empty
 [[ -z "$etc_puppet_modules" ]]      && puppetfile=true
-# .. or if HIMLAR_PUPPETFILE is set to 'deploy'
 [[ $HIMLAR_PUPPETFILE = "deploy" ]] && puppetfile=true
-# .. or if puppetmodules.sh is given the 'deploy' parameter
 [[ $1 = "deploy" ]]                 && puppetfile=true
+[[ "$puppetfile" = "true" ]] && provision_from_puppetfile
 
-# If /opt/himlar/modules contains modules from rsync, use them
-[[ -n "$opt_himlar_modules" ]]      && override=true
-
-[[ "$puppetfile" = "true" ]] && provision_from_puppetfile || true
-[[ "$override" = "true" ]]   && override_modules          || true
+# If /opt/himlar/modules contains a module deployed via rsync, use that instead
+# of the one in /etc/puppet/modules
+[[ -z "$opt_himlar_modules" ]] || override_modules
 
