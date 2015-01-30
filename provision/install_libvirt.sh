833e48fee7794dd9ab2fb59d7e47c5419cdbe992
edpto <tor.ladre@it.uib.no>
2015-01-30 11:04:39 +0100

    Added conditionals for el6

--- provision/install_libvirt.sh
+++ provision/install_libvirt.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+
+if [ -n "$(cat /etc/redhat-release | grep release\ 7)" ]; then
 # CentOS 7, Fedora 20/21
-sudo yum -y install qemu-kvm libvirt virt-install bridge-utils libvirt-daemon-kvm
-sudo systemctl start libvirtd
-sudo systemctl enable libvirtd
+  sudo yum -y install qemu-kvm libvirt virt-install bridge-utils libvirt-daemon-kvm
+  sudo systemctl start libvirtd
+  sudo systemctl enable libvirtd
+else
+  echo "$rhelversion"
+  sudo yum -y install qemu-kvm libvirt virt-install bridge-utils
+  chkconfig libvirtd on
+  service libvirtd start
+fi
