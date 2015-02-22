a1c754f99889351c6da36b03ca6c6a04945fbd99
Jan Ivar Beddari <code@beddari.net>
2015-02-22 08:43:44 +0100

    SSL fixes
    * Added DNS alt names to puppetmaster cert
    * puppetmaster now true in default Foreman data
    * Removing redundant default values from Foreman hieradata

--- provision/foreman-vagrant-up.sh
+++ provision/foreman-vagrant-up.sh
@@ -3,3 +3,5 @@
 HIMLAR_INTERN=true HIMLAR_CERTNAME=vagrant-foreman-dev.vagrant.local vagrant up base --provider=libvirt
 vagrant ssh base -c "sudo /opt/himlar/provision/foreman-vagrant-setup.sh changeme"
 
+# Run puppet to generate a report to Foreman
+vagrant ssh base -c "sudo puppet agent -t"
