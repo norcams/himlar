ae19d406e01d32958f74f495bb2ab87483d6e257
Mikael Dalsgard <mikael.dalsgard@usit.uio.no>
2015-02-26 14:03:45 +0100

    1. og ennå uffulstendige forsøk på "ordentlig" foreman-setup-skript for Bergen

--- provision/foreman-bgo-setup.sh
+++ provision/foreman-bgo-setup.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+
+# Register Foreman host in DNS and CNAMEs admin and puppet
+echo "update add bgo-foreman-bootstrap.mgmt.iaas.intern. 300 A 172.16.32.11
+      update add admin.mgmt.iaas.intern. 3600 CNAME bgo-foreman-bootstrap.mgmt.iaas.intern.
+      update add puppet.mgmt.iaas.intern. 3600 CNAME bgo-foreman-bootstrap.mgmt.iaas.intern.
+      send" | nsupdate -k /etc/rndc.key
+
+#
+# Foreman settings
+#
+rootpw='himlar-changeme'
+rootpw_md5=$(openssl passwd -1 $rootpw)
+echo '
+  Setting["root_pass"]        = "'$rootpw_md5'"
+  Setting["entries_per_page"] = 100
+  Setting["foreman_url"]      = "https://admin.mgmt.iaas.intern."
+  Setting["unattended_url"]   = "http://admin.mgmt.iaas.intern."
+' | foreman-rake console
+
+#
+# Network configuration objects
+#
 hammer domain create --name "bmc.iaas.intern"
 hammer domain info --name "bmc.iaas.intern"
 hammer domain create --name "mgmt.iaas.intern"
@@ -11,7 +33,6 @@ hammer subnet create --name "mgmt" \
   --gateway "172.16.32.1" \
   --dns-primary "129.177.6.54" \
   --dns-secondary "129.177.12.31"
-
 hammer subnet update --name "mgmt" \
   --domain-ids $(hammer domain list | grep "mgmt.iaas.intern" | head -c1)
 hammer subnet update --name "mgmt" \
@@ -30,28 +51,37 @@ hammer subnet create --name "bmc" \
   --gateway "172.16.24.1" \
   --dns-primary "129.177.6.54" \
   --dns-secondary "129.177.12.31"
-
 hammer subnet update --name "bmc" \
   --domain-ids $(hammer domain list | grep "bmc.iaas.intern" | head -c1)
 hammer domain update --name bmc.iaas.intern --dns-id $(hammer proxy list | grep foreman | head -c2)
 
+#
+# Puppet settings
+#
+# Enable Puppetlabs repo to enable Puppet bootstrap logic in kickstart template
+hammer global-parameter set --name enable-puppetlabs-repo --value true
+# Set up Puppet environment and import classes from proxy id 1
 hammer environment create --name "production"
 hammer environment info --name "production"
+hammer proxy import-classes --environment "production" --id $(hammer proxy list | grep foreman | head -c2)
 
-wget -P /tmp http://folk.uib.no/edpto/provision_openstack.erb
-wget -P	/tmp http://folk.uib.no/edpto/provision_kickstart_ifs.erb
-
-hammer template create --name "Kickstart_openstack" --type provision --file /tmp/provision_openstack.erb
-hammer template create --name "Kickstart default PXELinux ifs" --type PXELinux --file /tmp/provision_kickstart_ifs.erb
-
+#
+# Provisioning and discovery setup
+#
+# Create OS
 hammer os create --name CentOS --major 7 --minor 0.1406 --description "CentOS 7.0" --family Redhat \
   --architecture-ids 1 \
   --medium-ids $(hammer medium list | grep CentOS | head -c1) \
   --ptable-ids $(hammer partition-table list | grep "Kickstart default" | head -c1) 
 
+# Download and create templates
+wget -P /tmp http://folk.uib.no/edpto/provision_openstack.erb
+wget -P	/tmp http://folk.uib.no/edpto/provision_kickstart_ifs.erb
+hammer template create --name "Kickstart_openstack" --type provision --file /tmp/provision_openstack.erb
+hammer template create --name "Kickstart default PXELinux ifs" --type PXELinux --file /tmp/provision_kickstart_ifs.erb
+# Assign templates to OS
 hammer template update --name "Kickstart default PXELinux ifs" --operatingsystem-ids 1
 hammer template update --name "Kickstart_openstack" --operatingsystem-ids 1
-
 hammer os set-default-template --id 1 \
   --config-template-id $(hammer template list --per-page 10000 | grep "Kickstart_openstack"|cut -d" " -f1) 
 hammer os set-default-template --id 1 \
@@ -59,18 +89,16 @@ hammer os set-default-template --id 1 \
 hammer os update --id 1 \
   --ptable-ids $(hammer partition-table list --per-page 10000 | grep "Kickstart default" | cut -d" " -f1)
 
-hammer proxy import-classes --environment "production" --id $(hammer proxy list | grep 127 | head -c2) 
+# Create a base hostgroup with all services on proxy id 1
+hammer hostgroup create --name "base" --architecture "x86_64" --domain-id 1 --environment "production" --operatingsystem-id 1 --medium "CentOS mirror" --ptable "Kickstart default" --subnet "mgmt" --puppet-proxy-id 1 --puppet-ca-proxy-id 1
 
 # Get our custom provision templates
 foreman-rake templates:sync repo="https://github.com/norcams/community-templates.git" branch="norcams"
-
-# Find id for safemode_render
-safemode=$(curl -s -S -k -u admin:changeme http://127.0.0.1/api/v2/settings/?search="Safemode_render" | grep id | cut -d ',' -f1 | sed 's/[^0-9]*//g')
-
-# Set Safemode_render to true or false - return true or false
-settingid=$(echo $(curl -s -S -k -u admin:changeme http://127.0.0.1/api/v2/settings/$safemode/?search="Safemode_render"?page="true") | cut -d ',' -f1 | sed 's/[^0-9]*//g') && myresult=$(curl -s -S -k -u admin:changeme -X PUT -H "Content-Type:application/json" -H "Accept:application/json,version=2" -d "{\"setting\": {\"value\": \"false\"}}" http://127.0.0.1/api/v2/settings/$settingid) && echo "$myresult" | sed -e 's/.*value//g' | tr -dc '[:alnum:]\n\r'
-
-# Deploy new default pxe config to smart proxy
-curl -k -u admin:changeme http://127.0.0.1/api/v2/config_templates/build_pxe_default
-
-settingid=$(echo $(curl -s -S -k -u admin:changeme http://127.0.0.1/api/v2/settings/$safemode/?search="Safemode_render"?page="true") | cut -d ',' -f1 | sed 's/[^0-9]*//g') && myresult=$(curl -s -S -k -u admin:changeme -X PUT -H "Content-Type:application/json" -H "Accept:application/json,version=2" -d "{\"setting\": {\"value\": \"true\"}}" http://127.0.0.1/api/v2/settings/$settingid) && echo "$myresult" | sed -e 's/.*value//g' | tr -dc '[:alnum:]\n\r'
+# Render pxe default with safe mode rendering false - this is required for the discovery image
+# to get the foreman_url setting passed through to the kernel from the template
+echo '
+  Setting["safemode_render"] = false
+  include Foreman::Renderer
+  ConfigTemplate.build_pxe_default(self)
+  Setting["safemode_render"] = true
+' | foreman-rake console
