e8feb77c1e861979f771539e1f3e5e4e432eae61
Mikael Dalsgard <mikael.dalsgard@usit.uio.no>
2015-02-28 12:49:04 +0100

    Et mer komplett oppsett av bootstrap-utgaven av Foreman på UiB

--- provision/foreman-bgo-setup.sh
+++ provision/foreman-bgo-setup.sh
@@ -2,7 +2,8 @@
 
 
 # Register Foreman host in DNS and CNAMEs admin and puppet
-echo "update add bgo-foreman-bootstrap.mgmt.iaas.intern. 300 A 172.16.32.11
+echo "server alfalfa.uib.no
+      update add bgo-foreman-bootstrap.mgmt.iaas.intern. 300 A 172.16.32.11
       update add admin.mgmt.iaas.intern. 3600 CNAME bgo-foreman-bootstrap.mgmt.iaas.intern.
       update add puppet.mgmt.iaas.intern. 3600 CNAME bgo-foreman-bootstrap.mgmt.iaas.intern.
       send" | nsupdate -k /etc/rndc.key
@@ -15,15 +16,15 @@ rootpw_md5=$(openssl passwd -1 $rootpw)
 echo '
   Setting["root_pass"]        = "'$rootpw_md5'"
   Setting["entries_per_page"] = 100
-  Setting["foreman_url"]      = "https://admin.mgmt.iaas.intern."
-  Setting["unattended_url"]   = "http://admin.mgmt.iaas.intern."
+  Setting["foreman_url"]      = "https://admin.mgmt.iaas.intern"
+  Setting["unattended_url"]   = "http://admin.mgmt.iaas.intern"
 ' | foreman-rake console
 
 #
 # Network configuration objects
 #
-hammer domain create --name "bmc.iaas.intern"
-hammer domain info --name "bmc.iaas.intern"
+hammer domain create --name "oob.iaas.intern"
+hammer domain info --name "oob.iaas.intern"
 hammer domain create --name "mgmt.iaas.intern"
 hammer domain info --name "mgmt.iaas.intern"
 
@@ -36,24 +37,24 @@ hammer subnet create --name "mgmt" \
 hammer subnet update --name "mgmt" \
   --domain-ids $(hammer domain list | grep "mgmt.iaas.intern" | head -c1)
 hammer subnet update --name "mgmt" \
-  --dhcp-id $(hammer proxy list | grep foreman | head -c2)
+  --dhcp-id 1
 hammer subnet update --name "mgmt" \
-  --tftp-id $(hammer proxy list | grep foreman | head -c2)
+  --tftp-id 1
 hammer subnet update --name "mgmt" \
-  --dns-id $(hammer proxy list | grep foreman | head -c2)
+  --dns-id 1
 hammer subnet info --name "mgmt"
 hammer subnet update --name "mgmt" --from 172.16.32.200 --to 172.16.32.250
-hammer domain update --name mgmt.iaas.intern --dns-id $(hammer proxy list | grep foreman | head -c2)
+hammer domain update --name mgmt.iaas.intern --dns-id 1
 
-hammer subnet create --name "bmc" \
+hammer subnet create --name "oob" \
   --network "172.16.24.0" \
   --mask "255.255.248.0" \
   --gateway "172.16.24.1" \
   --dns-primary "129.177.6.54" \
   --dns-secondary "129.177.12.31"
-hammer subnet update --name "bmc" \
-  --domain-ids $(hammer domain list | grep "bmc.iaas.intern" | head -c1)
-hammer domain update --name bmc.iaas.intern --dns-id $(hammer proxy list | grep foreman | head -c2)
+hammer subnet update --name "oob" \
+  --domain-ids $(hammer domain list | grep "oob.iaas.intern" | head -c1)
+hammer domain update --name oob.iaas.intern --dns-id 1
 
 #
 # Puppet settings
@@ -63,11 +64,15 @@ hammer global-parameter set --name enable-puppetlabs-repo --value true
 # Set up Puppet environment and import classes from proxy id 1
 hammer environment create --name "production"
 hammer environment info --name "production"
-hammer proxy import-classes --environment "production" --id $(hammer proxy list | grep foreman | head -c2)
+hammer proxy import-classes --environment "production" --id 1
 
 #
 # Provisioning and discovery setup
 #
+
+# TEMP FIX wrong url for some CentOS mirrors throug mirror.centos.org http://projects.theforeman.org/issues/6884
+hammer medium update --id 1 --path 'http://centos.uib.no/$version/os/$arch'
+
 # Create OS
 hammer os create --name CentOS --major 7 --minor 0.1406 --description "CentOS 7.0" --family Redhat \
   --architecture-ids 1 \
@@ -90,7 +95,7 @@ hammer os update --id 1 \
   --ptable-ids $(hammer partition-table list --per-page 10000 | grep "Kickstart default" | cut -d" " -f1)
 
 # Create a base hostgroup with all services on proxy id 1
-hammer hostgroup create --name "base" --architecture "x86_64" --domain-id 1 --environment "production" --operatingsystem-id 1 --medium "CentOS mirror" --ptable "Kickstart default" --subnet "mgmt" --puppet-proxy-id 1 --puppet-ca-proxy-id 1
+hammer hostgroup create --name "base" --architecture "x86_64" --domain-id 1 --environment "production" --operatingsystem-id 1 --medium "CentOS mirror" --ptable "Kickstart default" --subnet-id 1 --puppet-proxy-id 1 --puppet-ca-proxy-id 1
 
 # Get our custom provision templates
 foreman-rake templates:sync repo="https://github.com/norcams/community-templates.git" branch="norcams"
