dd02f5466624c63a57344d7d447f171e00eb1ebe
Jan Ivar Beddari <code@beddari.net>
2015-03-30 11:59:06 +0200

    FIX: Removing obsolete provisioning scripts

--- provision/foreman-bgo-up.sh
+++
@@ -1,6 +0,0 @@
-# Assumes libvirt provider
-HIMLAR_BRIDGE=bond0.909 HIMLAR_BRIDGE2=bond0.908 HIMLAR_CERTNAME=bgo-foreman-bootstrap.mgmt.iaas.intern vagrant up base --provider=libvirt
-vagrant ssh base -c "sudo /opt/himlar/provision/foreman-bgo-setup.sh changeme"
-
-# Run puppet to generate a report to Foreman
-vagrant ssh base -c "sudo puppet agent -t"
