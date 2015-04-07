d76d824bc23a19dd3f6f13f166d7ba3d07b8ec7d
Jan Ivar Beddari <code@beddari.net>
2015-04-07 11:23:37 +0200

    Test import of templates using only foreman_templates plugin

--- provision/foreman-osl-setup.sh
+++ provision/foreman-osl-setup.sh
@@ -1,5 +1,32 @@
-#!/bin/bash
+#!/bin/bash -xv
 
+
+# Register Foreman host in DNS and CNAMEs admin and puppet
+echo "server 129.240.2.6
+      update add osl-foreman-1.iaas.uio.no. 3600 A 129.240.224.101
+      update add admin.iaas.uio.no. 3600 CNAME osl-foreman-1.iaas.uio.no.
+      update add puppet.iaas.uio.no. 3600 CNAME osl-foreman-1.iaas.uio.no.
+      send" | nsupdate -k /etc/rndc.key
+
+#echo "server 129.240.2.6
+#      update add 101.224.240.129.in-addr.arpa. 3600 PTR osl-foreman-1.iaas.uio.no.
+#      send" | nsupdate -k /etc/rndc.key
+
+#
+# Foreman settings
+#
+rootpw='Himlarchangeme'
+rootpw_md5=$(openssl passwd -1 $rootpw)
+echo '
+  Setting["root_pass"]        = "'$rootpw_md5'"
+  Setting["entries_per_page"] = 100
+  Setting["foreman_url"]      = "https://osl-foreman-1.iaas.uio.no"
+  Setting["unattended_url"]   = "http://osl-foreman-1.iaas.uio.no"
+' | foreman-rake console
+
+#
+# Network configuration objects
+#
 hammer domain create --name "iaas.uio.no"
 hammer domain info --name "iaas.uio.no"
 
@@ -13,62 +40,51 @@ hammer subnet create --name "mgmt" \
 hammer subnet update --name "mgmt" \
   --domain-ids $(hammer domain list | grep "iaas.uio.no" | head -c1)
 hammer subnet update --name "mgmt" \
-  --dhcp-id $(hammer proxy list | grep foreman | head -c2)
+  --dhcp-id 1
 hammer subnet update --name "mgmt" \
-  --tftp-id $(hammer proxy list | grep foreman | head -c2)
+  --tftp-id 1
 hammer subnet update --name "mgmt" \
-  --dns-id $(hammer proxy list | grep foreman | head -c2)
-hammer subnet info --name "mgmt"
+  --dns-id 1
 hammer subnet update --name "mgmt" --from 129.240.224.115 --to 129.240.224.126
+hammer subnet info --name "mgmt"
 
-hammer subnet create --name "oob" \
-  --network "129.240.224.64" \
-  --mask "255.255.255.224" \
-  --gateway "129.240.224.65" \
-  --dns-primary "129.240.2.3" \
-  --dns-secondary "129.240.2.40"
-
-hammer subnet update --name "oob" \
-  --domain-ids $(hammer domain list | grep "iaas.uio.no" | head -c1)
-
+#
+# Puppet settings
+#
+# Enable Puppetlabs repo to enable Puppet bootstrap logic in kickstart template
+hammer global-parameter set --name enable-puppetlabs-repo --value true
+# Set up Puppet environment and import classes from proxy id 1
 hammer environment create --name "production"
 hammer environment info --name "production"
+hammer proxy import-classes --environment "production" --id 1
 
-wget -P /tmp https://github.com/norcams/community-templates/blob/norcams/kickstart/norcams_provision.erb
-wget -P /tmp https://github.com/norcams/community-templates/blob/norcams/kickstart/norcams_PXELinux.erb
-wget -P /tmp https://github.com/norcams/community-templates/blob/norcams/kickstart/norcams_disklayout.erb
+#
+# Provisioning and discovery setup
+#
 
-hammer partition-table create --name "Kickstart partition instdev" --os-family Redhat --file /tmp/norcams_disklayout.erb
-hammer template create --name "Kickstart_openstack" --type provision --file /tmp/norcams_provision.erb
-hammer template create --name "Kickstart default PXELinux ifs" --type PXELinux --file /tmp/norcams_PXELinux.erb
-
+# Create OS
 hammer os create --name CentOS --major 7 --minor 0.1406 --description "CentOS 7.0" --family Redhat \
   --architecture-ids 1 \
-  --medium-ids $(hammer medium list | grep CentOS | head -c1) \
-  --ptable-ids $(hammer partition-table list | grep "Kickstart partition instdev" | head -c1) 
-
-hammer template update --name "Kickstart default PXELinux ifs" --operatingsystem-ids 1
-hammer template update --name "Kickstart_openstack" --operatingsystem-ids 1
-
-hammer os set-default-template --id 1 \
-  --config-template-id $(hammer template list --per-page 10000 | grep "Kickstart_openstack"|cut -d" " -f1) 
-hammer os set-default-template --id 1 \
-  --config-template-id $(hammer template list --per-page 10000 | grep "Kickstart default PXELinux ifs" | cut -d" " -f1)
-hammer os update --id 1 \
-  --ptable-ids $(hammer partition-table list --per-page 10000 | grep "Kickstart default" | cut -d" " -f1)
-
-hammer proxy import-classes --environment "production" --id $(hammer proxy list | grep 127 | head -c2) 
+  --medium-ids $(hammer medium list | grep CentOS | head -c1)
 
 # Get our custom provision templates
-foreman-rake templates:sync repo="https://github.com/norcams/community-templates.git" branch="norcams"
+foreman-rake templates:sync repo="https://github.com/norcams/community-templates.git" branch="0.1.1" associate="always"
 
-# Find id for safemode_render
-safemode=$(curl -s -S -k -u admin:$1 http://127.0.0.1/api/v2/settings/?search="Safemode_render" | grep id | cut -d ',' -f1 | sed 's/[^0-9]*//g')
+hammer os set-default-template --id 1 \
+  --config-template-id $(hammer template list --per-page 10000 | grep "norcams Kickstart default"|cut -d" " -f1)
+hammer os set-default-template --id 1 \
+  --config-template-id $(hammer template list --per-page 10000 | grep "norcams PXELinux default" |cut -d" " -f1)
+hammer os update --id 1 \
+  --ptable-ids $(hammer partition-table list --per-page 10000  | grep "norcams ptable default"   |cut -d" " -f1)
 
-# Set Safemode_render to true or false - return true or false
-settingid=$(echo $(curl -s -S -k -u admin:$1 http://127.0.0.1/api/v2/settings/$safemode/?search="Safemode_render"?page="true") | cut -d ',' -f1 | sed 's/[^0-9]*//g') && myresult=$(curl -s -S -k -u admin:$1 -X PUT -H "Content-Type:application/json" -H "Accept:application/json,version=2" -d "{\"setting\": {\"value\": \"false\"}}" http://127.0.0.1/api/v2/settings/$settingid) && echo "$myresult" | sed -e 's/.*value//g' | tr -dc '[:alnum:]\n\r'
+# Create a base hostgroup with all services on proxy id 1
+hammer hostgroup create --name "base" --architecture "x86_64" --domain-id 1 --environment "production" --operatingsystem-id 1 --medium "CentOS mirror" --ptable "norcams ptable default" --subnet-id 1 --puppet-proxy-id 1 --puppet-ca-proxy-id 1
 
-# Deploy new default pxe config to smart proxy
-curl -k -u admin:$1 http://127.0.0.1/api/v2/config_templates/build_pxe_default
-
-settingid=$(echo $(curl -s -S -k -u admin:$1 http://127.0.0.1/api/v2/settings/$safemode/?search="Safemode_render"?page="true") | cut -d ',' -f1 | sed 's/[^0-9]*//g') && myresult=$(curl -s -S -k -u admin:$1 -X PUT -H "Content-Type:application/json" -H "Accept:application/json,version=2" -d "{\"setting\": {\"value\": \"true\"}}" http://127.0.0.1/api/v2/settings/$settingid) && echo "$myresult" | sed -e 's/.*value//g' | tr -dc '[:alnum:]\n\r'
+# Render pxe default with safe mode rendering false - this is required for the discovery image
+# to get the foreman_url setting passed through to the kernel from the template
+echo '
+  Setting["safemode_render"] = false
+  include Foreman::Renderer
+  ConfigTemplate.build_pxe_default(self)
+  Setting["safemode_render"] = true
+' | foreman-rake console
