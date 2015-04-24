92728dd60d1f6db6a4c1c85369bf9ccfa2a632e2
Jan Ivar Beddari <code@beddari.net>
2015-04-24 22:44:27 +0200

    Updated foreman-vagrant-up.sh helper script

--- provision/foreman-vagrant-up.sh
+++ provision/foreman-vagrant-up.sh
@@ -1,7 +1,11 @@
-# Assumes libvirt provider
+#!/bin/bash -x
+set -o errexit
 
+# Assumes libvirt provider
 HIMLAR_INTERN=true HIMLAR_CERTNAME=vagrant-foreman-dev.himlar.local vagrant up base --provider=libvirt
-vagrant ssh base -c "sudo /opt/himlar/provision/foreman-vagrant-setup.sh changeme"
+
+# Run settings script to configure Foreman
+vagrant ssh base -c "sudo /opt/himlar/provision/foreman-settings.sh"
 
 # Run puppet to generate a report to Foreman
 vagrant ssh base -c "sudo puppet agent -t"
