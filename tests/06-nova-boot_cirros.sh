cb56cbd95fc236c2377efc99157ec703a8a820fe
Erlend Midttun <erlend.midttun@ntnu.no>
2015-09-03 11:59:42 +0200

    New Openstack version has changes in command line tools

--- tests/06-nova-boot_cirros.sh
+++ tests/06-nova-boot_cirros.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -vx
 IMAGE_ID=$(nova image-list | awk '/CirrOS/ { print $2 }' | head -n 1)
 NET_ID=$(neutron net-show testnet | grep " id " | awk '{ print $4 }')
-nova boot --flavor 1 --image ${IMAGE_ID} --nic net-id=${NET_ID} --security_group test test-1
+nova boot --flavor 1 --image ${IMAGE_ID} --nic net-id=${NET_ID} --security_group test_sec_group test-1
