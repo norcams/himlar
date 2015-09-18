ef1b6a4f7e7218cc94ade2b618726e48a11358a3
Jan Ivar Beddari <code@beddari.net>
2015-09-18 10:14:31 +0200

    yaml syntax checker
    Updated to check all files, or check files provided as command
    line options

--- hieradata/syntax-check.sh
+++ hieradata/syntax-check.sh
@@ -1,11 +1,18 @@
 #!/bin/bash -eu
 
 validate_yaml() {
-  ruby -ryaml -e "YAML.load_file '$1'"
+  while [[ $# -gt 0 ]]; do
+    ruby -ryaml -e "YAML.load_file '$1'"
+    shift
+  done
 }
 
-while [[ $# -gt 0 ]]; do
-  validate_yaml "$1"
-  shift
-done
+# if we got params, validate them, if not, validate all
+if [[ $# -gt 0 ]]; then
+  validate_yaml "$@"
+else
+  export -f validate_yaml
+  find . -type f -name *.yaml -print0 | xargs -0 bash -c 'validate_yaml "$@"'
+fi
+
 
