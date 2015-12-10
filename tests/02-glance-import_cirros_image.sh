5302e050b48035d555da279957783fda69229a89
edpto <tor.ladre@it.uib.no>
2015-12-10 10:44:59 +0100

    Updated image import test for liberty and cirros version

--- tests/02-glance-import_cirros_image.sh
+++ tests/02-glance-import_cirros_image.sh
@@ -2,7 +2,7 @@
 set -o errexit
 source ~/openstack.config
 if [ ! -f /tmp/cirros.img ]; then
-  curl -#o /tmp/cirros.img http://download.cirros-cloud.net/0.3.3/cirros-0.3.3-x86_64-disk.img
+  curl -#o /tmp/cirros.img http://download.cirros-cloud.net/0.3.4/cirros-0.3.4-x86_64-disk.img
 fi
 # Does not work for some reason?
 #osadmin image create "CirrOS test image" --disk-format qcow2 --public --file /tmp/cirros.img
@@ -10,7 +10,7 @@ fi
 source ~/keystonerc_admin
 glance image-create --name "CirrOS test image" \
   --disk-format qcow2 --container-format bare \
-  --is-public=True \
+  --visibility public \
   --file /tmp/cirros.img
 nova image-list
 
