7862aa2445f8c3e40704b8ed3bea244e1b3d0fde
edpto <tor.ladre@it.uib.no>
2016-02-04 18:35:45 +0100

    Added basic osd service management

--- profile/manifests/storage/cephosd.pp
+++ profile/manifests/storage/cephosd.pp
@@ -3,5 +3,14 @@
 #
 class profile::storage::cephosd {
   include ::ceph::profile::osd
-}
 
+  service { 'ceph-osd':
+    name      => 'ceph',
+    ensure    => running,
+    enable    => true,
+    provider  => 'init',
+    status    => "/sbin/service ceph status osd",
+    start     => "/sbin/service ceph start osd",
+    restart   => "/sbin/service ceph start osd",
+  }
+}
