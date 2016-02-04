9aa816da2748c1a1b6074d6188668fcd384608dd
edpto <tor.ladre@it.uib.no>
2016-02-04 22:14:01 +0100

    Improve service control and trigger initial osd start

--- profile/manifests/storage/cephosd.pp
+++ profile/manifests/storage/cephosd.pp
@@ -8,9 +8,23 @@ class profile::storage::cephosd {
     name      => 'ceph',
     ensure    => running,
     enable    => true,
-    provider  => 'init',
+    provider  => redhat,
     status    => "/sbin/service ceph status osd",
     start     => "/sbin/service ceph start osd",
     restart   => "/sbin/service ceph start osd",
+    require   => Class[ceph::osds],
+  }
+
+  file { '/var/lib/ceph-configured':
+    ensure    => file,
+    require   => Class[ceph::osds],
+    notify    => Exec['initial start of osds'],
+  }
+
+  # ceph service must be started once before service status works
+  exec { 'initial start of osds':
+    command     => '/sbin/service ceph start osd',
+    require     => File['/var/lib/ceph-configured'],
+    refreshonly => true,
   }
 }
