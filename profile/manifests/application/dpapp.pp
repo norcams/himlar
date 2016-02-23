7cc9f40fa0c706cddb788434d254ad0de5568cbc
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2016-02-23 13:34:46 +0100

    Added profile for dpapp

---
+++ profile/manifests/application/dpapp.pp
@@ -0,0 +1,30 @@
+#
+class profile::application::dpapp(
+  $manage_firewall = true
+) {
+
+  include ::dpapp
+
+  profile::firewall::rule { '190 dpapp-http accept tcp':
+    port   => [ 80, 6543 ],
+    destination => $::ipaddress_public1,
+    extras => {
+      ensure => $manage_firewall? { true => present , default => absent }
+    }
+  }
+
+  profile::firewall::rule { '191 forward http to 6543 http':
+    chain  => 'PREROUTING',
+    port   => 80,
+    destination => $::ipaddress_public1,
+    extras => {
+      ensure => $manage_firewall? { true => present , default => absent },
+      jump => 'REDIRECT',
+      toports => '6543',
+      table => 'nat',
+      action => undef,
+      state => undef
+    }
+  }
+
+}
