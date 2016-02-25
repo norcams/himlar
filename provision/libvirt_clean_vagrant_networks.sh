fe39142f82ff7aeba3ede78562a168ed198b4616
Jan Ivar Beddari <code@beddari.net>
2016-02-25 15:30:05 +0100

    FIX: s/destroy/net-destroy/

--- provision/libvirt_clean_vagrant_networks.sh
+++ provision/libvirt_clean_vagrant_networks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ux
 
 networks=$(sudo virsh net-list --name | grep -E '^(himlar.*|vagrant-libvirt)$')
-echo $networks | xargs -n1 sudo virsh destroy
+echo $networks | xargs -n1 sudo virsh net-destroy
 echo $networks | xargs -n1 sudo virsh net-undefine
 
