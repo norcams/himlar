bae5da3336e14fd22d497ad292f7cf563142e6c1
Trond Hasle Amundsen <t.h.amundsen@usit.uio.no>
2016-03-07 15:24:43 +0100

    fix test 00 and 01

--- tests/00-credentials_setup.sh
+++ tests/00-credentials_setup.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-cat <<'END_CONFIG' > ~/openstack.config
-os_url="http://10.0.3.14:35357/v2.0"
+cat <<"END_CONFIG" > ~/openstack.config
+os_url="http://172.31.24.20:35357/v3"
 os_token="admintoken"
-ostoken()
+broken_osadmin()
 {
   openstack --os-token $os_token --os-url $os_url "$@"
 }
 
-os_auth_url="http://10.0.3.14:5000/v2.0"
-os_username="demo"
-os_password="himlar0pen"
-os_project_name="demoproject"
+os_auth_url="http://172.31.24.20:5000/v3"
+os_demo_username="demo"
+os_demo_password="himlar0pen"
+os_demo_project_name="demoproject"
 osdemo()
 {
-  openstack --os-username $os_username --os-password $os_password --os-project-name $os_project_name --os-auth-url $os_auth_url "$@"
+  openstack --os-username $os_demo_username --os-password $os_demo_password --os-project-name $os_demo_project_name --os-auth-url $os_auth_url "$@"
 }
 END_CONFIG
 
@@ -23,11 +23,11 @@ cat <<'END_DEMO' > ~/keystonerc_demo
 export OS_USERNAME=demo
 export OS_TENANT_NAME=demoproject
 export OS_PASSWORD=himlar0pen
-export OS_AUTH_URL=http://10.0.3.14:5000/v3
+export OS_AUTH_URL=http://172.31.24.20:5000/v3
 export OS_IDENTITY_API_VERSION=3
 export OS_USER_DOMAIN_NAME=${OS_USER_DOMAIN_NAME:-"Default"}
 export OS_PROJECT_DOMAIN_NAME=${OS_PROJECT_DOMAIN_NAME:-"Default"}
-export PS1='[\u@\h \W(keystone_admin)]\$ '
+export PS1='\[\e[0;34m\][\u@\h \[\e[0;35m\]\W \[\e[0;33m\](demo)\[\e[0;34m\]]\$\[\e[0m\] '
 export OS_NO_CACHE=1
 alias openstack="/usr/bin/openstack"
 unset http_proxy
@@ -39,12 +39,12 @@ cat <<'END_ADMIN' > ~/keystonerc_admin
 export OS_USERNAME=admin
 export OS_TENANT_NAME=openstack
 export OS_PASSWORD=admin_pass
-export OS_REGION_NAME=dev01
-export OS_AUTH_URL=http://10.0.3.14:5000/v3
+export OS_REGION_NAME=vagrant
+export OS_AUTH_URL=http://172.31.24.20:5000/v3
 export OS_IDENTITY_API_VERSION=3
 export OS_USER_DOMAIN_NAME=${OS_USER_DOMAIN_NAME:-"Default"}
 export OS_PROJECT_DOMAIN_NAME=${OS_PROJECT_DOMAIN_NAME:-"Default"}
-export PS1='[\u@\h \W(keystone_admin)]$ '
+export PS1='\[\e[0;34m\][\u@\h \[\e[0;35m\]\W \[\e[0;31m\](admin)\[\e[0;34m\]]\$\[\e[0m\] '
 export OS_NO_CACHE=1
 alias openstack="/usr/bin/openstack"
 unset http_proxy
