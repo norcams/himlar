a21a2a22dc03fa28746d671c6e54196f075dc0c0
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2016-03-08 15:30:16 +0100

    Use service address in test script

--- tests/00-credentials_setup.sh
+++ tests/00-credentials_setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 cat <<"END_CONFIG" > ~/openstack.config
-os_url="http://172.31.24.20:35357/v3"
+os_url="http://172.31.16.20:35357/v3"
 os_token="admintoken"
 broken_osadmin()
 {
   openstack --os-token $os_token --os-url $os_url "$@"
 }
 
-os_auth_url="http://172.31.24.20:5000/v3"
+os_auth_url="http://172.31.16.20:5000/v3"
 os_demo_username="demo"
 os_demo_password="himlar0pen"
 os_demo_project_name="demoproject"
@@ -23,7 +23,7 @@ cat <<'END_DEMO' > ~/keystonerc_demo
 export OS_USERNAME=demo
 export OS_TENANT_NAME=demoproject
 export OS_PASSWORD=himlar0pen
-export OS_AUTH_URL=http://172.31.24.20:5000/v3
+export OS_AUTH_URL=http://172.31.16.20:5000/v3
 export OS_IDENTITY_API_VERSION=3
 export OS_USER_DOMAIN_NAME=${OS_USER_DOMAIN_NAME:-"Default"}
 export OS_PROJECT_DOMAIN_NAME=${OS_PROJECT_DOMAIN_NAME:-"Default"}
@@ -40,7 +40,7 @@ export OS_USERNAME=admin
 export OS_TENANT_NAME=openstack
 export OS_PASSWORD=admin_pass
 export OS_REGION_NAME=vagrant
-export OS_AUTH_URL=http://172.31.24.20:5000/v3
+export OS_AUTH_URL=http://172.31.16.20:5000/v3
 export OS_IDENTITY_API_VERSION=3
 export OS_USER_DOMAIN_NAME=${OS_USER_DOMAIN_NAME:-"Default"}
 export OS_PROJECT_DOMAIN_NAME=${OS_PROJECT_DOMAIN_NAME:-"Default"}
