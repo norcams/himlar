4f5223ca600d61a16a2f1e7085e6e8c04e0f26f7
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2016-03-08 10:14:57 +0100

    Provision scripts for secrets

---
+++ provision/puppetsecrets.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+function usage {
+  echo "ONLY RUN THIS INSIDE AN OPENSTACK LOCATION!"
+  echo ""
+  echo "Run as root after the SSH keys are distributed with ansible"
+  echo ""
+}
+
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   usage
+   exit 1
+fi
+
+# Make sure you have the git SSH keys
+if [ ! -f /root/.ssh/git_rsa ] || [ ! -f /root/.ssh/git_rsa.pub ]; then
+  usage
+  exit 1
+fi
+
+
+secrets_dir='/opt/repo/secrets/hieradata'
+mkdir -p ${secrets_dir}
+
+if [ -f ${secrets_dir}/.git ]; then
+  cd ${secrets_dir} && git pull
+else
+  git clone git:hieradata/secrets ${secrets_dir}
+fi
+
+rsync -avh ${secrets_dir}/* /opt/himlar/hieradata/secrets/
+
+apachectl graceful
