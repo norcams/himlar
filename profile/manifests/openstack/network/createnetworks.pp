db4bd5bb3f4b79ee3dedfcc85945784e9a335398
edpto <tor.ladre@it.uib.no>
2016-03-31 13:46:20 +0200

    Add support for subnets

--- profile/manifests/openstack/network/createnetworks.pp
+++ profile/manifests/openstack/network/createnetworks.pp
@@ -5,4 +5,5 @@ class profile::openstack::network::createnetworks {
   include ::lvm
 
   create_resources(neutron_network, hiera('profile::openstack::createnetworks::networks', {}))
+  create_resources(neutron_subnet, hiera('profile::openstack::createnetworks::subnets', {}))
 }
