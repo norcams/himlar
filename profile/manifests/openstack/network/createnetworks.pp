498496ac831e4cb6a3a4eb5b600544592e9e75cf
edpto <tor.ladre@it.uib.no>
2016-03-31 14:06:41 +0200

    Remove meaningless include from copy-paste error

--- profile/manifests/openstack/network/createnetworks.pp
+++ profile/manifests/openstack/network/createnetworks.pp
@@ -2,7 +2,6 @@
 #
 #
 class profile::openstack::network::createnetworks {
-  include ::lvm
 
   create_resources(neutron_network, hiera('profile::openstack::createnetworks::networks', {}))
   create_resources(neutron_subnet, hiera('profile::openstack::createnetworks::subnets', {}))
