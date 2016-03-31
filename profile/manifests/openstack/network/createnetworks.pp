98d46b0f797e21f464f1df6544784fa8742ef1ae
edpto <tor.ladre@it.uib.no>
2016-03-31 14:31:27 +0200

    Create profile resource and new hiera merge strategy

--- profile/manifests/openstack/network/createnetworks.pp
+++
@@ -1,8 +0,0 @@
-# Class: profile::openstack::network::createnetworks
-#
-#
-class profile::openstack::network::createnetworks {
-
-  create_resources(neutron_network, hiera('profile::openstack::createnetworks::networks', {}))
-  create_resources(neutron_subnet, hiera('profile::openstack::createnetworks::subnets', {}))
-}
