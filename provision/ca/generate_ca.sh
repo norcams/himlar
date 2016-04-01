e89b637294020ba8e35aa308ec8d70c52fb5d34c
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2016-04-01 21:59:44 +0200

    Added root CA to use in vagrant

---
+++ provision/ca/generate_ca.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+/bin/openssl genrsa -out private/ca.key.pem 4096
+/bin/openssl req -config root.cnf \
+      -key private/ca.key.pem \
+      -new -x509 -days 7300 -sha256 -extensions v3_ca \
+      -out certs/ca.cert.pem
+
+openssl x509 -noout -text -in certs/ca.cert.pem
+
+cp -f certs/ca.cert.pem certs/ca-chain.cert.pem
