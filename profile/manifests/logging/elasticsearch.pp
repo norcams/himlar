e60e40692864f0bcae40155622eee4aef4265049
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2016-04-07 12:47:26 +0200

    Updated rsyslog and added logstash and elasticsearch

--- profile/manifests/logging/elasticsearch.pp
+++ profile/manifests/logging/elasticsearch.pp
@@ -1,6 +1,21 @@
 #
-class profile::logging::elasticsearch() {
+class profile::logging::elasticsearch(
+  $instances = {},
+  $manage_firewall = true,
+  $ports = [9200]
+) {
 
   include ::elasticsearch
+
+  create_resources('elasticsearch::instance', $instances)
+
+  if $manage_firewall {
+    $allow_from_network = hiera_array('allow_from_network')
+    profile::firewall::rule { '400 elasticsearch accept tcp':
+      port    => $ports,
+      source  => $ipaddress_mgmt1,
+      extras  => $firewall_extras,
+    }
+  }
 
 }
--- profile/manifests/logging/logstash.pp
+++ profile/manifests/logging/logstash.pp
@@ -3,4 +3,11 @@ class profile::logging::logstash() {
 
   include ::logstash
 
+  file { '/etc/logstash/patterns/extra':
+    ensure => present,
+    content => template("${module_name}/logging/logstash/patterns.erb")
+  } ->
+  ::logstash::configfile { 'openstack':
+    content => template("${module_name}/logging/logstash/openstack.conf.erb")
+  }
 }
