724011641332f09f57155ea50218d214ecc338ce
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2016-04-11 11:28:32 +0200

    Added new working elasticsearch template for logstash

--- profile/manifests/logging/logstash.pp
+++ profile/manifests/logging/logstash.pp
@@ -7,6 +7,10 @@ class profile::logging::logstash() {
     ensure => present,
     content => template("${module_name}/logging/logstash/patterns.erb")
   } ->
+  file { '/etc/logstash/elasticsearch-template.json':
+    ensure => present,
+    content => template("${module_name}/logging/logstash/elasticsearch-template.json")
+  }
   ::logstash::configfile { 'openstack':
     content => template("${module_name}/logging/logstash/openstack.conf.erb")
   }
