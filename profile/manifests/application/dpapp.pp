e7905997b6205946d3936e83b73a4661c0a7dce2
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2016-04-12 14:17:13 +0200

    Run dpapp with mod_wsgi

--- profile/manifests/application/dpapp.pp
+++ profile/manifests/application/dpapp.pp
@@ -8,25 +8,11 @@ class profile::application::dpapp(
   $allow_from_network = hiera_array('allow_from_network')
 
   profile::firewall::rule { '190 dpapp-http accept tcp':
-    port   => [ 80, 6543 ],
+    port   => [ 80, 443 ],
     destination => $::ipaddress_public1,
     source      => $allow_from_network,
     extras => {
       ensure => $manage_firewall? { true => present , default => absent }
-    }
-  }
-
-  profile::firewall::rule { '191 forward http to 6543 http':
-    chain  => 'PREROUTING',
-    port   => 80,
-    destination => $::ipaddress_public1,
-    extras => {
-      ensure => $manage_firewall? { true => present , default => absent },
-      jump => 'DNAT',
-      todest => "$::ipaddress_public1:6543",
-      table => 'nat',
-      action => undef,
-      state => undef
     }
   }
 
