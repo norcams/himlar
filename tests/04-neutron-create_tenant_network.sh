5ea8f5c07cd9027bf4e702492178bb8eb2e17bef
Trond Hasle Amundsen <t.h.amundsen@usit.uio.no>
2016-04-13 14:02:56 +0200

    possibly irrelevant neutron test for vagrant updated slightly

--- tests/04-neutron-create_tenant_network.sh
+++ tests/04-neutron-create_tenant_network.sh
@@ -1,4 +1,14 @@
 #!/bin/bash -x
 set -o errexit
+
+source ~/keystonerc_admin
+
 neutron net-create testnet
 neutron subnet-create --name testsubnet testnet 10.200.0.0/24
+
+# NEW:
+## Create a network "testnet"
+#openstack network create testnet
+#
+## Create a subnet "testsubnet" which is a member of "testnet"
+#neutron subnet-create --name testsubnet testnet 10.200.0.0/24
