a563fc1fd81707ccf77085dfc72be88ce582787b
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2016-04-18 10:31:45 +0200

    Updated include in proxy api

--- profile/manifests/openstack/api.pp
+++ profile/manifests/openstack/api.pp
@@ -4,7 +4,7 @@ class profile::openstack::api(
   $default_vhost_config = {}
 ) {
 
-  include apache::mod::headers
+  include ::apache::mod::headers
 
   create_resources('profile::openstack::api::proxy', $vhosts)
 
