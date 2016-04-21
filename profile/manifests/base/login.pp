12754ddbb47cd4611b5c8549975e775f49fc96d4
Mikael Dalsgard <mikael.dalsgard@usit.uio.no>
2016-04-21 10:23:02 +0200

    Bugfix (RPM package changed location and name some time ago)

--- profile/manifests/base/login.pp
+++ profile/manifests/base/login.pp
@@ -14,7 +14,7 @@ class profile::base::login
     create_resources('googleauthenticator::pam', $pam_modules)
   }
 
-  package { 'norcams-ga':
+  package { 'uio-google-authenticator':
     provider => 'rpm',
     ensure   => 'installed',
     source   => 'http://rpm.uio.no/uio-free/rhel/7Server/x86_64/uio-google-authenticator-0.3-1.20160314git750d40d.el7.x86_64.rpm'
