646a8273c87552f7be6a34b79f734ec1bcf03ed0
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2016-04-26 17:44:43 +0200

    Updated logger in vagrant

--- profile/manifests/logging/elasticsearch.pp
+++ profile/manifests/logging/elasticsearch.pp
@@ -10,14 +10,12 @@ class profile::logging::elasticsearch(
 
   create_resources('elasticsearch::instance', $instances)
 
-  info($instances)
-
   if $manage_firewall {
     $allow_from_network = hiera_array('allow_from_network')
     profile::firewall::rule { '400 elasticsearch accept tcp':
       port        => $ports,
       destination => $::ipaddress_mgmt1,
-      source      => "${::network_mgmt1}/24",
+      source      => "${::network_mgmt1}/${::netmask_mgmt1}",
       extras      => $firewall_extras,
     }
   }
--- profile/manifests/logging/logstash.pp
+++ profile/manifests/logging/logstash.pp
@@ -3,14 +3,6 @@ class profile::logging::logstash() {
 
   include ::logstash
 
-  file { '/etc/logstash/patterns/extra':
-    ensure => present,
-    content => template("${module_name}/logging/logstash/patterns.erb")
-  } ->
-  file { '/etc/logstash/elasticsearch-template.json':
-    ensure => present,
-    content => template("${module_name}/logging/logstash/elasticsearch-template.json")
-  }
   ::logstash::configfile { 'openstack':
     content => template("${module_name}/logging/logstash/openstack.conf.erb")
   }
