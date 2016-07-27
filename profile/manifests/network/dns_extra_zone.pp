d6de1a5eb5488830ed4a46e1c6298a42ebd11ebc
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2016-07-27 08:53:33 +0200

    Added profile class to add optional dns zones

---
+++ profile/manifests/network/dns_extra_zone.pp
@@ -0,0 +1,15 @@
+#
+# This will only work if the dns module is already set up.
+# E.G. foreman_proxy with managed dns server
+#
+class profile::network::dns_extra_zone(
+  $zone,
+) {
+
+  ::dns::zone { $zone:
+    soa     => $::fqdn,
+    reverse => false,
+    soaip   => $::ipaddress_mgmt1
+  }
+
+}
