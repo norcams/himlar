3ec64c4a5debbae6128fdc26efdfe4bd68a7571c
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2016-09-23 10:23:42 +0200

    Fixed broken sensu agent enable/disable

--- profile/manifests/monitoring/sensu/agent.pp
+++ profile/manifests/monitoring/sensu/agent.pp
@@ -5,8 +5,10 @@ class profile::monitoring::sensu::agent (
   $checks  = {},
 ) {
 
-  include ::sensu
+  if $enable_agent {
+    include ::sensu
 
-  create_resources('::sensu::plugin', $plugins)
-  create_resources('::sensu::check', $checks)
+    create_resources('::sensu::plugin', $plugins)
+    create_resources('::sensu::check', $checks)
+  }
 }
