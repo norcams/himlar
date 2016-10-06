2513a2c08a2a481758a08370c3c2b2e9931e4da0
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2016-10-06 20:19:58 +0200

    Added senus metric and checks to mariadb. Default turned off

--- profile/manifests/monitoring/sensu/agent.pp
+++ profile/manifests/monitoring/sensu/agent.pp
@@ -1,12 +1,13 @@
 #
 class profile::monitoring::sensu::agent (
   $enable_agent = false,
-  $plugins = {},
-  $checks  = {},
 ) {
 
   if $enable_agent {
     include ::sensu
+
+    $plugins = hiera_hash('profile::monitoring::sensu::agent::plugins', {})
+    $checks  = hiera_hash('profile::monitoring::sensu::agent::checks', {})
 
     create_resources('::sensu::plugin', $plugins)
     create_resources('::sensu::check', $checks)
