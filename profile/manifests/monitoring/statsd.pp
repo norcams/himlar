60e49cf3a40fc5160679bb0adc85396c1b60b464
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2016-10-17 19:27:56 +0200

    Added statsd frontend to graphite on monitor

---
+++ profile/manifests/monitoring/statsd.pp
@@ -0,0 +1,17 @@
+#
+class profile::monitoring::statsd(
+  $manage_firewall           = false,
+  $firewall_extras           = {}
+) {
+
+  include ::statsd
+
+  if $manage_firewall {
+    profile::firewall::rule { '413 statsd accept udp':
+      port        => 8125,
+      proto       => 'udp',
+      destination => $::ipaddress_mgmt1,
+      extras      => $firewall_extras,
+    }
+  }
+}
