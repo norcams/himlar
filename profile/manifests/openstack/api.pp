ff4f49899fc496a88e8631568202e12dc7f53b94
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2016-10-20 15:33:44 +0200

    Added option to manage apache for api profile class

--- profile/manifests/openstack/api.pp
+++ profile/manifests/openstack/api.pp
@@ -1,8 +1,13 @@
 # Profile for the public api service
 class profile::openstack::api(
   $vhosts,
+  $manage_apache = false,
   $default_vhost_config = {}
 ) {
+
+  if $manage_apache {
+      include ::apache
+  }
 
   include ::apache::mod::headers
 
