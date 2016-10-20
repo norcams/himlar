f68e5cd7d0c8adbc2bae718b755ee9e932f86cac
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2016-10-20 09:04:40 +0200

    Added option to manage horizon on dashboard

--- profile/manifests/openstack/dashboard.pp
+++ profile/manifests/openstack/dashboard.pp
@@ -1,15 +1,24 @@
 # Dashboard
 class profile::openstack::dashboard(
-  $manage_ssl_cert = false,
-  $ports = [80,443,5000,8773,8774,8776,9292,9696],
-  $manage_firewall = true,
-  $service_net = "${::network_service1}/${::netmask_service1}",
-  $firewall_extras = {},
-  $manage_overrides = true,
-  $override_template = "${module_name}/openstack/horizon/local_settings.erb",
-  $site_branding = 'UH-IaaS'
+  $manage_dashboard   = false,
+  $manage_ssl_cert    = false,
+  $ports              = [80,443,5000,8773,8774,8776,9292,9696],
+  $manage_firewall    = false,
+  $internal_net       = "${::network_trp1}/${::netmask_trp1}",
+  $firewall_extras    = {},
+  $manage_overrides   = false,
+  $override_template  = "${module_name}/openstack/horizon/local_settings.erb",
+  $site_branding      = 'UH-IaaS'
 ) {
-  include ::horizon
+
+  if $manage_dashboard {
+    include ::horizon
+    concat::fragment { 'extra-local_settings.py':
+      target  => $::horizon::params::config_file,
+      content => template($override_template),
+      order   => '99',
+    }
+  }
 
   if $manage_ssl_cert {
     include profile::application::sslcert
@@ -24,9 +33,9 @@ class profile::openstack::dashboard(
       source => $allow_from_network,
       extras => $firewall_extras,
     }
-    profile::firewall::rule { '236 service openstack-dashboard and api accept tcp':
+    profile::firewall::rule { '236 internal openstack-dashboard and api accept tcp':
       port   => $ports,
-      source => $service_net,
+      source => $internal_net,
       extras => $firewall_extras,
     }
   }
@@ -38,10 +47,4 @@ class profile::openstack::dashboard(
       notify => Service['httpd']
     }
   }
-  concat::fragment { 'extra-local_settings.py':
-    target  => $::horizon::params::config_file,
-    content => template($override_template),
-    order   => '99',
-  }
-
 }
