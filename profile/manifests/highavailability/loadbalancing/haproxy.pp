c12d1fea51d2de4b1e80a6b89ba948c41895311b
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2016-11-01 19:40:24 +0100

    Added new node api with haproxy and vagrant setup

--- profile/manifests/highavailability/loadbalancing/haproxy.pp
+++ profile/manifests/highavailability/loadbalancing/haproxy.pp
@@ -1,13 +1,8 @@
 #
-# Author: Yanis Guenane <yanis@guenane.org>
-# License: ApacheV2
-#
-# Puppet module :
-#   mod 'puppetlabs/haproxy'
-#   mod 'puppetlabs/stdlib'
-#   mod 'puppetlabs/concat'
-#
 class profile::highavailability::loadbalancing::haproxy (
+  $manage_haproxy          = false,
+  $manage_firewall         = false,
+  $firewall_extras         = {},
   $haproxy_listens         = {},
   $haproxy_frontends       = {},
   $haproxy_backends        = {},
@@ -16,13 +11,29 @@ class profile::highavailability::loadbalancing::haproxy (
   $haproxy_peers           = {},
 ) {
 
-  include ::haproxy
+  if $manage_haproxy {
 
-  create_resources('haproxy::listen', $haproxy_listens)
-  create_resources('haproxy::backend', $haproxy_backends)
-  create_resources('haproxy::frontend', $haproxy_frontends)
-  create_resources('haproxy::balancermember', $haproxy_balancermembers)
-  create_resources('haproxy::userlist', $haproxy_userlists)
-  create_resources('haproxy::peer', $haproxy_peers)
+    include ::haproxy
+
+    Haproxy::Listen {
+      collect_exported => false
+    }
+
+    create_resources('haproxy::listen', $haproxy_listens)
+    create_resources('haproxy::backend', $haproxy_backends)
+    create_resources('haproxy::frontend', $haproxy_frontends)
+    create_resources('haproxy::balancermember', $haproxy_balancermembers)
+    create_resources('haproxy::userlist', $haproxy_userlists)
+    create_resources('haproxy::peer', $haproxy_peers)
+
+  }
+
+  if $manage_firewall {
+    profile::firewall::rule { '450 haproxy accept tcp':
+      proto       => 'tcp',
+      destination => $::ipaddress_public1,
+      extras      => $firewall_extras,
+    }
+  }
 
 }
