6f2f1479b9332858a9b9321263677ccfc778ef67
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2017-01-16 16:53:08 +0100

    Added option to install sensu plugin from custom gem location

--- profile/manifests/monitoring/sensu/agent.pp
+++ profile/manifests/monitoring/sensu/agent.pp
@@ -6,10 +6,13 @@ class profile::monitoring::sensu::agent (
   if $enable_agent {
     include ::sensu
 
+    $gems = hiera_hash('profile::monitoring::sensu::agent::plugin_gems', {})
     $plugins = hiera_hash('profile::monitoring::sensu::agent::plugins', {})
     $checks  = hiera_hash('profile::monitoring::sensu::agent::checks', {})
 
     create_resources('::sensu::plugin', $plugins)
     create_resources('::sensu::check', $checks)
+    create_resources('package', $gems)
+
   }
 }
