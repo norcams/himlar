0811ea07380aadccd6fbb0e3156e85da823f55b4
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2017-01-18 09:23:29 +0100

    Manage default availability zone in nova.conf for compute and novactrl

--- profile/manifests/openstack/compute.pp
+++ profile/manifests/openstack/compute.pp
@@ -1,5 +1,13 @@
-class profile::openstack::compute {
+#
+class profile::openstack::compute(
+  $manage_az            = false
+) {
   include ::nova
   include ::nova::config
   include ::nova::network::neutron
+
+  if $manage_az {
+    include ::nova::availability_zone
+  }
+
 }
