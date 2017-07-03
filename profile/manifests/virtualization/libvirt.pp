d403b1d4b4c9c7c281a3f4e5810ac9f0a0936a5b
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2017-07-03 11:57:36 +0200

    Use dport in firewall rules for libvirt

--- profile/manifests/virtualization/libvirt.pp
+++ profile/manifests/virtualization/libvirt.pp
@@ -21,17 +21,17 @@ class profile::virtualization::libvirt(
 
   if $manage_firewall {
     profile::firewall::rule { '180 libvirt-tcp accept tcp':
-      port   => 16509,
+      dport  => 16509,
       extras => $firewall_extras['tcp']
     }
 
     profile::firewall::rule { '181 libvirt-tls accept tcp':
-      port   => 16514,
+      dport  => 16514,
       extras => $firewall_extras['tls']
     }
 
     profile::firewall::rule { '182 libvirt-graphics console accept tcp':
-      port   => '5900-5999',
+      dport  => '5900-5999',
       source => "${::network_mgmt1}/${::netmask_mgmt1}",
       extras => $firewall_extras['graphics']
     }
