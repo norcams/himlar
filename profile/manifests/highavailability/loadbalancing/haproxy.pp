fa1eea7329345547480bc13c0523100bfca9d166
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2017-07-11 12:52:09 +0200

    Added mapfile to haproxy profile

--- profile/manifests/highavailability/loadbalancing/haproxy.pp
+++ profile/manifests/highavailability/loadbalancing/haproxy.pp
@@ -10,6 +10,7 @@ class profile::highavailability::loadbalancing::haproxy (
   $haproxy_balancermembers = {},
   $haproxy_userlists       = {},
   $haproxy_peers           = {},
+  $haproxy_mapfile         = {}
 ) {
 
   if $manage_haproxy {
@@ -20,11 +21,16 @@ class profile::highavailability::loadbalancing::haproxy (
       collect_exported => false
     }
 
+    Haproxy::Backend {
+      collect_exported => false
+    }
+
     create_resources('haproxy::listen', $haproxy_listens)
     create_resources('haproxy::backend', $haproxy_backends)
     create_resources('haproxy::frontend', $haproxy_frontends)
     create_resources('haproxy::balancermember', $haproxy_balancermembers)
     create_resources('haproxy::userlist', $haproxy_userlists)
+    create_resources('haproxy::mapfile', $haproxy_mapfile)
     create_resources('haproxy::peer', $haproxy_peers)
 
     file { '/etc/haproxy/sorry.http':
@@ -32,6 +38,12 @@ class profile::highavailability::loadbalancing::haproxy (
       source => "puppet:///modules/${module_name}/loadbalancing/haproxy.sorry.http",
       notify => Service['haproxy']
     }
+
+    file { '/root/watch-status.sh':
+      ensure  => present,
+      mode    => '0755',
+      content => "watch 'echo \"show stat\" | nc -U /var/lib/haproxy/stats | cut -d \",\" -f 1,2,5-11,18,24,27,30,36,50,37,56,57,62 | column -s, -t'"
+  }
 
   }
 
