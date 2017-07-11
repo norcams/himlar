96ad9255eb6beb756814148bca431b558c5ed06b
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2017-07-11 12:36:51 +0200

    Added corosync and ha role

---
+++ profile/manifests/highavailability/corosync.pp
@@ -0,0 +1,32 @@
+#
+# Management of both the software stack (pacemaker and corosync)
+# and the cluster resources
+#
+class profile::highavailability::corosync(
+  $manage_corosync = false,
+  $manage_firewall = false,
+  $properties      = {},
+  $primitives      = {},
+  $locations       = {},
+  $clones          = {},
+  $firewall_extras = {}
+) {
+
+  if $manage_corosync {
+    include ::corosync
+
+    create_resources('cs_property', $properties)
+    create_resources('cs_primitive', $primitives)
+    create_resources('cs_location', $locations)
+    create_resources('cs_clone', $clones)
+  }
+
+  if $manage_firewall {
+    profile::firewall::rule { '460 corosync udp':
+      proto       => 'udp',
+      dport       => '5405',
+      destination => $::ipaddress_public1,
+      extras      => $firewall_extras
+    }
+  }
+}
