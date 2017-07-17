2820fa6cc18febc6ffae4723de5ae5d1b6cd6ae0
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2017-07-17 10:23:09 +0200

    Use manage_firewall option on rsyslog server

--- profile/manifests/logging/rsyslog/server.pp
+++ profile/manifests/logging/rsyslog/server.pp
@@ -6,19 +6,21 @@ class profile::logging::rsyslog::server(
 
   include ::rsyslog::server
 
+  if $manage_firewall {
 
-  profile::firewall::rule { '150 rsyslog accept udp':
-    port        => [ 514 ],
-    destination => $::ipaddress_mgmt1,
-    proto       => 'udp',
-    extras      => $firewall_extras,
-  }
+    profile::firewall::rule { '150 rsyslog accept udp':
+      port        => [ 514 ],
+      destination => $::ipaddress_mgmt1,
+      proto       => 'udp',
+      extras      => $firewall_extras,
+    }
 
-  profile::firewall::rule { '150 rsyslog accept tcp':
-    port        => [ 514 ],
-    destination => $::ipaddress_mgmt1,
-    proto       => 'tcp',
-    extras      => $firewall_extras,
+    profile::firewall::rule { '150 rsyslog accept tcp':
+      port        => [ 514 ],
+      destination => $::ipaddress_mgmt1,
+      proto       => 'tcp',
+      extras      => $firewall_extras,
+    }
   }
 
 }
