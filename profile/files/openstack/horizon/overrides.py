eb624cafa94bb9951e17c7bcea94b5d5d12f0e78
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2017-07-26 16:53:43 +0200

    Only hide network panel i horizon for non-admin and remove change password

--- profile/files/openstack/horizon/overrides.py
+++ profile/files/openstack/horizon/overrides.py
@@ -13,17 +13,28 @@ tables.SimpleDisassociateIP.allowed = NO
 
 project_dashboard = horizon.get_dashboard("project")
 
-# Completely remove panel Network->Routers
+# Hide panel Network->Routers
 routers_panel = project_dashboard.get_panel("routers")
-project_dashboard.unregister(routers_panel.__class__)
+permissions = list(getattr(routers_panel, 'permissions', []))
+permissions.append('openstack.roles.admin')
+routers_panel.permissions = tuple(permissions)
 
-# Completely remove panel Network->Networks
+# Hide panel Network->Networks for all but admin
 networks_panel = project_dashboard.get_panel("networks")
-project_dashboard.unregister(networks_panel.__class__)    # Disable Floating IPs
+permissions = list(getattr(networks_panel, 'permissions', []))
+permissions.append('openstack.roles.admin')
+networks_panel.permissions = tuple(permissions)
 
-# Completely remove panel Network->Network Topology
+# Hide panel Network->Network Topology for all but admin
 topology_panel = project_dashboard.get_panel("network_topology")
-project_dashboard.unregister(topology_panel.__class__)
+permissions = list(getattr(topology_panel, 'permissions', []))
+permissions.append('openstack.roles.admin')
+topology_panel.permissions = tuple(permissions)
+
+# Remove change password
+settings = horizon.get_dashboard("settings")
+password_panel = settings.get_panel("password")
+settings.unregister(password_panel.__class__)
 
 # Remove "Volume Consistency Groups" tab
 from openstack_dashboard.dashboards.project.volumes import tabs
