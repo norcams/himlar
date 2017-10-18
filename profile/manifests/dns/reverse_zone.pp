941b2ecc2cf47bbfde9a0fe2d1884adb340b21ab
Trond Hasle Amundsen <t.h.amundsen@usit.uio.no>
2017-10-18 12:42:46 +0200

    fix travis warning

--- profile/manifests/dns/reverse_zone.pp
+++ profile/manifests/dns/reverse_zone.pp
@@ -1,6 +1,6 @@
 define profile::dns::reverse_zone($cidr, $origin, $filename) {
   $internal_zone = $::profile::dns::ns::internal_zone
-  file { "/var/named/$filename":
+  file { "/var/named/${filename}":
     content      => template("${module_name}/dns/bind/reverse_zone.erb"),
     notify       => Service['named'],
     mode         => '0640',
