0d2d63a6a4a9599c143dce3a5595efbd77a6eeec
TorLdre <tor.ledre@gmail.com>
2018-04-11 15:13:42 +0200

    enable option for bird6

--- profile/manifests/dns/ns.pp
+++ profile/manifests/dns/ns.pp
@@ -2,6 +2,7 @@ class profile::dns::ns (
   $allowed_nets = undef,
   $check_named_health = false,
   $enable_bird = false,
+  $enable_bird6 = false,
   $my_mgmt_addr = {},
   $my_transport_addr = {},
   #$mdns_transport_addr = {},
@@ -13,6 +14,7 @@ class profile::dns::ns (
   $authoritative = {},
   $manage_firewall = {},
   $manage_bird_firewall = false,
+  $manage_bird6_firewall = false,
   $firewall_extras = {},
   $public_zone = {},
   $forward_everything = false,
@@ -161,26 +163,54 @@ class profile::dns::ns (
   }
   if $manage_bird_firewall {
     profile::firewall::rule { '912 bird allow bfd':
-      proto  => 'udp',
-      port   => ['3784','3785','4784','4785'],
+      proto    => 'udp',
+      port     => ['3784','3785','4784','4785'],
     }
     profile::firewall::rule { "010 bird bgp - accept tcp to ${name}":
-      proto   => 'tcp',
-      port    => '179',
-      iniface => $::ipaddress_trp1,
+      proto    => 'tcp',
+      port     => '179',
+      iniface  => $::ipaddress_trp1,
+    }
+  }
+  if $enable_bird6 {
+    package { 'bird6':
+      ensure   => installed
+    }
+    file { '/etc/bird6.conf':
+      ensure   => file,
+      content  => template("${module_name}/bird/bird-resolver.conf6.erb"),
+      notify   => Service['bird6']
+    }
+    service { 'bird6':
+      ensure   => running,
+      enable   => true,
+      require  => Package['bird6']
+    }
+  }
+  if $manage_bird6_firewall {
+    profile::firewall::rule { '912 bird allow bfd ipv6':
+      proto    => 'udp',
+      port     => ['3784','3785','4784','4785'],
+      provider => 'ip6tables',
+    }
+    profile::firewall::rule { "010 bird bgp ipv6 - accept tcp to ${name}":
+      proto    => 'tcp',
+      port     => '179',
+      provider => 'ip6tables',
+      iniface  => $::ipaddress6_trp1,
     }
   }
   if $check_named_health {
     file { '/opt/named-checks/':
-      ensure  => directory,
+      ensure   => directory,
     } ~>
     file { "named_check.sh":
-      ensure  => present,
-      owner   => root,
-      group   => root,
-      mode    => '0755',
-      path    => "/opt/named-checks/named_health.sh",
-      content => template("${module_name}/dns/bind/named_check.erb"),
+      ensure   => present,
+      owner    => root,
+      group    => root,
+      mode     => '0755',
+      path     => "/opt/named-checks/named_health.sh",
+      content  => template("${module_name}/dns/bind/named_check.erb"),
     }
   }
 }
