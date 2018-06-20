f68ce1643b1640d0c41228c070f6c31368a60a42
Anders Vaage <anders@unix.uib.no>
2018-06-20 10:08:32 +0200

    profile::openstack::compute::api and profile::openstack::compute::placement -> profile::openstack::novactrl

--- profile/manifests/openstack/novactrl.pp
+++ profile/manifests/openstack/novactrl.pp
@@ -6,14 +6,15 @@ class profile::openstack::novactrl(
   $enable_consoleproxy  = false,
   $enable_conductor     = false,
   $manage_quotas        = false,
-  $manage_az            = false
+  $manage_az            = false,
+  $manage_firewall      = false,
+  $firewall_extras      = {}
 ) {
 
+  include ::nova
+  include ::nova::placement
   include ::nova::config
-
-  if $enable_api {
-    include ::profile::openstack::compute::api
-  }
+  include ::nova::network::neutron
 
   if $enable_scheduler {
     include ::profile::openstack::compute::scheduler
@@ -38,5 +39,24 @@ class profile::openstack::novactrl(
   if $manage_az {
     include ::nova::availability_zone
   }
+
+  if $manage_firewall {
+    profile::firewall::rule { '220 nova-placement-api accept tcp':
+      port   => 80,
+      extras => $firewall_extras
+    }
+
+    profile::firewall::rule { '220 nova-api accept tcp':
+      port   => 8774,
+      extras => $firewall_extras
+    }
+
+    profile::firewall::rule { '220 nova-api-ec2 accept tcp':
+      port   => 8773,
+      extras => $firewall_extras
+    }
+
+  }
+
 
 }
