4cb07f4e006f1c2482126f9f4d035a8bc037b8a6
Trond Hasle Amundsen <t.h.amundsen@usit.uio.no>
2018-07-06 11:23:54 +0200

    fix enable_designate/designate_enabled confusion

--- profile/manifests/openstack/dashboard.pp
+++ profile/manifests/openstack/dashboard.pp
@@ -12,7 +12,7 @@ class profile::openstack::dashboard(
   $change_uploaddir     = false,
   $custom_uploaddir     = '/image-upload',
   $enable_pwd_retrieval = false,
-  $enable_designate     = false,
+  $designate_enabled    = false,
   $image_upload_mode    = undef,
   $change_region_selector = false,
   $change_login_footer  = false,
@@ -64,7 +64,7 @@ class profile::openstack::dashboard(
   }
 
   # Designate plugin
-  if $enable_designate {
+  if $designate_enabled {
     file { '/usr/share/openstack-dashboard/openstack_dashboard/local/enabled/_1710_project_dns_panel_group.py':
       ensure => present,
       source => 'file:///usr/lib/python2.7/site-packages/designatedashboard/enabled/_1710_project_dns_panel_group.py',
