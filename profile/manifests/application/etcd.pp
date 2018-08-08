71a725323a9e1491c412207b0a0aa4aab3e40576
Anders Vaage <anders@unix.uib.no>
2018-08-08 16:20:52 +0200

    variable for enabling installation of etcd bootstrap script, fix syntax error

--- profile/manifests/application/etcd.pp
+++ profile/manifests/application/etcd.pp
@@ -1,19 +1,22 @@
 class profile::application::etcd(
   $manage_firewall = true,
   $firewall_extras = {},
+  $install_bootstrap_script = false,
   $etcd_name = lookup('etcd::etcd_name', String, 'first', ''),
-  $listen_client_urls = lookup('etcd::listen_client_urls', Array, 'deep', undef),
-  $advertise_client_urls = lookup('etcd::advertise_client_urls', Array, 'deep', undef),
-  $listen_peer_urls = lookup('etcd::listen_peer_urls', Array, 'deep', undef),
-  $initial_advertise_peer_urls = lookup('etcd::initial_advertise_peer_urls', Array, 'deep', undef),
-  $initial_cluster = lookup('etcd::initial_cluster', Array, 'deep', undef),
+  $listen_client_urls = lookup('etcd::listen_client_urls', Array, 'deep', []),
+  $advertise_client_urls = lookup('etcd::advertise_client_urls', Array, 'deep', []),
+  $listen_peer_urls = lookup('etcd::listen_peer_urls', Array, 'deep', []),
+  $initial_advertise_peer_urls = lookup('etcd::initial_advertise_peer_urls', Array, 'deep', []),
+  $initial_cluster = lookup('etcd::initial_cluster', Array, 'deep', []),
 ){
   include ::etcd
 
-  file { '/usr/local/sbin/bootstrap-etcd-member':
-    ensure  => file,
-    content => template("${module_name}/application/etcd/bootstrap-member.erb"),
-    mode    => '0755'
+  if $install_bootstrap_script {
+    file { '/usr/local/sbin/bootstrap-etcd-member':
+      ensure  => file,
+      content => template("${module_name}/application/etcd/bootstrap-member.erb"),
+      mode    => '0755'
+    }
   }
 
   if $manage_firewall {
