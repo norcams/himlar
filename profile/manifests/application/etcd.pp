a67a5169628a716f9b879e540b318705c14a4487
Anders Vaage <anders@unix.uib.no>
2018-08-08 12:32:10 +0200

    write a script for bootstrapping a new member to an etcd cluster

--- profile/manifests/application/etcd.pp
+++ profile/manifests/application/etcd.pp
@@ -1,8 +1,19 @@
 class profile::application::etcd(
   $manage_firewall = true,
   $firewall_extras = {},
+  $etcd_name = lookup('etcd::etcd_name', String, 'first', ''),
+  $listen_client_urls = lookup('etcd::listen_client_urls', Array, 'deep', undef),
+  $advertise_client_urls = lookup('etcd::advertise_client_urls', Array, 'deep', undef),
+  $listen_peer_urls = lookup('etcd::listen_peer_urls', Array, 'deep', undef),
+  $initial_advertise_peer_urls = lookup('etcd::initial_advertise_peer_urls', Array, 'deep', undef),
+  $initial_cluster = lookup('etcd::initial_cluster', Array, 'deep', undef),
 ){
   include ::etcd
+
+  file { '/usr/local/sbin/bootstrap-etcd-member':
+    ensure  => file,
+    content => template("${module_name}/application/etcd/bootstrap-member.erb"),
+  }
 
   if $manage_firewall {
         profile::firewall::rule{ '2379 etcd client accept tcp':
