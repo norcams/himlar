446cba99d32c3800683ffcf8231d18039ee9fa82
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2018-08-16 15:32:41 +0200

    Added more options to netdata profile class

--- profile/manifests/monitoring/netdata.pp
+++ profile/manifests/monitoring/netdata.pp
@@ -1,6 +1,7 @@
 #
 class profile::monitoring::netdata(
   $manage_firewall           = false,
+  $firewall_dest             = $::ipaddress_mgmt1,
   $firewall_extras           = {}
 ) {
 
@@ -10,7 +11,7 @@ class profile::monitoring::netdata(
     profile::firewall::rule { '466 netdata accept tcp':
       dport       => 19999,
       proto       => 'tcp',
-      destination => $::ipaddress_mgmt1,
+      destination => $firewall_dest,
       extras      => $firewall_extras,
     }
   }
