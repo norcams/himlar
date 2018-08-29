08806d98b31e10ad00f96f2a94ac468df4d30fe6
TorLdre <tor.ledre@gmail.com>
2018-08-29 13:46:51 +0200

    fix deprecation

--- profile/manifests/openstack/network/controller.pp
+++ profile/manifests/openstack/network/controller.pp
@@ -11,6 +11,7 @@ class profile::openstack::network::controller(
 
   include ::neutron::server
   include ::neutron::server::notifications
+  include ::neutron::config
 
   create_resources('neutron_config', $neutron_config)
 
