b9056e30a199bff05b8f5274199b7557a09a62ea
TorLdre <tor.ledre@gmail.com>
2018-09-10 13:41:15 +0200

    add ceph metadata server profile code

---
+++ profile/manifests/storage/cephmds.pp
@@ -0,0 +1,19 @@
+# Class: profile::storage::cephmds
+#
+#
+class profile::storage::cephmds (
+  $manage_firewall = false,
+  $firewall_extras = {
+    'mds_listen'   => {},
+  },
+) {
+
+  include ::ceph::profile::mds
+
+  if $manage_firewall {
+    profile::firewall::rule { '100 ceph-mds accept tcp':
+      dport  => ['6800-7300'],
+      extras => $firewall_extras['mds_listen']
+    }
+  }
+}
