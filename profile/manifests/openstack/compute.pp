87f7733efe3a3cd026b720d1e4b27cbe71283254
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2018-09-27 13:53:24 +0200

    Added wrapper script for dhcp lease file check

--- profile/manifests/openstack/compute.pp
+++ profile/manifests/openstack/compute.pp
@@ -1,7 +1,8 @@
 #
 class profile::openstack::compute(
-  $manage_az            = false,
-  $manage_telemetry     = false
+  $manage_az                    = false,
+  $manage_telemetry             = false,
+  $manage_check_dhcp_lease_file = false
 ) {
   include ::nova
   include ::nova::config
@@ -17,4 +18,12 @@ class profile::openstack::compute(
     include ::nova::availability_zone
   }
 
+  if $manage_check_dhcp_lease_file {
+    file { '/usr/local/bin/check-dhcp-lease-file.sh':
+      ensure => present,
+      mode   => '0755',
+      owner  => 'root',
+      source => "puppet:///modules/${module_name}/openstack/compute/check-dhcp-lease-file.sh",
+    }
+  }
 }
