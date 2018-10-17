00585d78302b0eea094f946a75568dc76a652a95
TorLdre <tor.ledre@gmail.com>
2018-10-17 10:24:59 +0200

    fix travis complaint

--- profile/manifests/base/physical.pp
+++ profile/manifests/base/physical.pp
@@ -27,7 +27,7 @@ class profile::base::physical (
     $bmc_network  = regsubst("${::ipaddress_trp1}", '^(\d+)\.(\d+)\.(\d+)\.(\d+)$','\2',) - 1
     $bmc_address  = regsubst("${::ipaddress_trp1}", '^(\d+)\.(\d+)\.(\d+)\.(\d+)$',"\\1.${bmc_network}.\\3.\\4",)
     $bmc_username = lookup("bmc_username", String, 'first', '')
-    $bmc_password = lookup("bmc_password_${location}", String, 'first', '')
+    $bmc_password = lookup("bmc_password_${::location}", String, 'first', '')
     if $enable_redfish_http_proxy {
       $http_proxy     = lookup('mgmt__address__proxy', String, 'first', '')
       $http_proxy_url = " --proxy1.0 ${http_proxy}:8888"
