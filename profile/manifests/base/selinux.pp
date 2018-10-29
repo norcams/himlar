fda5b65721962febf131f055db78a65da5126a6f
Trond Hasle Amundsen <t.h.amundsen@usit.uio.no>
2018-10-29 14:41:32 +0100

    run restorecon on /var/lib/nova/.ssh

--- profile/manifests/base/selinux.pp
+++ profile/manifests/base/selinux.pp
@@ -13,10 +13,12 @@ class profile::base::selinux(
     $ports = lookup('profile::base::selinux::ports', Hash, 'deep', {})
     $boolean = lookup('profile::base::selinux::boolean', Hash, 'deep', {})
     $fcontext = lookup('profile::base::selinux::fcontext', Hash, 'deep', {})
+    $fcontext = lookup('profile::base::selinux::exec_restorecon', Hash, 'deep', {})
 
     create_resources('selinux::port', $ports)
     create_resources('selinux::boolean', $boolean)
     create_resources('selinux::fcontext', $fcontext)
+    create_resources('selinux::exec_restorecon', $exec_restorecon)
 
     package { $packages:
       ensure => installed
