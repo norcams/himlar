629abe7bc45a2616c72fbe21196667b5e94f624d
Trond Hasle Amundsen <t.h.amundsen@usit.uio.no>
2018-11-20 13:15:51 +0100

    fix erroneous usage of lookup in class variable

--- profile/manifests/openstack/designate.pp
+++ profile/manifests/openstack/designate.pp
@@ -1,6 +1,6 @@
 class profile::openstack::designate (
   $manage_firewall = false,
-  $bind_servers = lookup('profile::openstack::designate::bind_servers', Hash, 'first', {})
+  $bind_servers = {}
 )
 {
   include ::designate
@@ -11,6 +11,8 @@ class profile::openstack::designate (
   include ::designate::config
   include ::designate::worker
   include ::designate::producer
+
+  $bind_servers = lookup('profile::openstack::designate::bind_servers', Hash, 'first', {})
 
   file { '/etc/designate/pools.yaml':
     content      => template("${module_name}/openstack/designate/pools.yaml.erb"),
