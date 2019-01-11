2f66f80a4460ddf151dfa5f00e0afbe772456c88
TorLdre <tor.ledre@gmail.com>
2019-01-11 10:40:50 +0100

    add foreman_proxy class

---
+++ profile/manifests/application/foreman_proxy.pp
@@ -0,0 +1,17 @@
+#
+# Move non-foreman stuff from admin here
+#
+class profile::application::foreman_proxy
+  $serve_oob_dhcp = false,
+) {
+
+  # requires an include statement in role yaml
+  if $serve_oob_dhcp {
+
+    file { 'oob_network.conf':
+      ensure   => file,
+      path     => '/etc/dhcp/oob_network.conf',
+      content  => template("${module_name}/application/foreman_proxy/oob_network.conf.erb"),
+    }
+  }
+}
