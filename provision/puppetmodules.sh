86863c26e0b3305773bdafd93dfc5e7aaf584f71
Mikael Dalsgard <mikaeld@iaas-vagrant02.uio.no>
2019-02-26 13:08:47 +0100

    Force module updates if outside vagrant, use local modules if available (if inside vagrant)

--- provision/puppetmodules.sh
+++ provision/puppetmodules.sh
@@ -4,15 +4,21 @@ LN_OPTS="-sfT"
 CODE_PATH=/etc/puppetlabs/code
 ENV_PATH=environments/production
 
+etc_puppet_modules="$(ls -d $CODE_PATH/modules/*/ 2>/dev/null)"
+opt_himlar_modules="$(ls -d /opt/himlar/modules/*/ 2>/dev/null)"
+host=$(hostname)
+environment=${host%%-*}     # assumes standard name convention with environment as first part (in front of a '-')
+
 provision_from_puppetfile()
 {
   ln $LN_OPTS /opt/himlar/manifests/site.pp $CODE_PATH/$ENV_PATH/manifests/site.pp
   rm -rf $CODE_PATH/$ENV_PATH/hieradata
   ln $LN_OPTS /opt/himlar/hieradata $CODE_PATH/$ENV_PATH/hieradata
   ln $LN_OPTS /opt/himlar/hiera.yaml $CODE_PATH/$ENV_PATH/hiera.yaml
+  if [[ ! "${environment}x" == "vagrantx" ]]; then force="--force"; fi
 
   cd /opt/himlar && /usr/local/bin/r10k --verbose 4 puppetfile purge
-  cd /opt/himlar && /usr/local/bin/r10k --verbose 4 puppetfile install --moduledir $CODE_PATH/modules --puppetfile /opt/himlar/Puppetfile
+  cd /opt/himlar && /usr/local/bin/r10k --verbose 4 puppetfile install --moduledir $CODE_PATH/modules --puppetfile /opt/himlar/Puppetfile ${force}
   # link in profile module after running r10k
   ln -sf /opt/himlar/profile $CODE_PATH/modules/
 }
@@ -25,9 +31,6 @@ override_modules()
     rm -rf $CODE_PATH/$(echo ${m#/opt/himlar/}) 2>/dev/null
   done
 }
-
-etc_puppet_modules="$(ls -d $CODE_PATH/modules/*/ 2>/dev/null)"
-opt_himlar_modules="$(ls -d /opt/himlar/modules/*/ 2>/dev/null)"
 
 # Source command line options as env vars
 while [[ $# -gt 0 ]]; do
@@ -51,6 +54,9 @@ puppetfile=false
 [[ $HIMLAR_PUPPETFILE = "deploy" ]] && puppetfile=true
 [[ "$puppetfile" = "true" ]] && provision_from_puppetfile
 
-# If /opt/himlar/modules contains a module deployed via rsync, use that instead
-# of the one in /etc/puppet/modules
-[[ -z "$opt_himlar_modules" ]] || override_modules
+# only consider module overriding from himlar if in environment 'vagrant' (only for development)
+if [[ "${environment}x" == "vagrantx" ]]; then
+  # If /opt/himlar/modules contains a module deployed via rsync, use that instead
+  # of the one in /etc/puppet/modules
+  [[ -z "$opt_himlar_modules" ]] || override_modules
+fi
