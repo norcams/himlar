cc71524b6e565e3911f55ecca11ebf8861bb1612
tanzr <tannazn@mail.uio.no>
2019-02-27 13:52:26 +0100

    removed irrelevant commits

--- profile/manifests/storage/cephmon.pp
+++ profile/manifests/storage/cephmon.pp
@@ -1,20 +1,45 @@
-# Class: profile::storage::cephmds
+# Class: profile::storage::cephmon
 #
 #
-class profile::storage::cephmds (
-  $manage_firewall = false,
-  $firewall_extras = {
-    'mds_listen'   => {},
-  },
+class profile::storage::cephmon (
+  $dashboard_password,
+  $dashboard_user       = 'admin',
+  $manage_dashboard     = false,
+  $crt_dir              = '/etc/pki/tls/certs',
+  $key_dir              = '/etc/pki/tls/private',
+  $cert_name            = $::fqdn,
+  $flagfile             = '/var/lib/ceph/.ceph-dashboard-set-up'
 ) {
+  include ::ceph::profile::mon
+  include ::ceph::profile::mgr
 
-  include ::ceph::profile::client
-  include ::ceph::profile::mds
+  if $manage_dashboard {
+    ceph_config {
+      'mon/mgr initial modules':    value => 'dashboard'
+    }
 
-  if $manage_firewall {
-    profile::firewall::rule { '100 ceph-mds accept tcp':
-      dport  => ['6800-7300'],
-      extras => $firewall_extras['mds_listen']
+    exec { 'dashboard.crt':
+      command => "ceph config-key set mgr/dashboard/crt -i ${crt_dir}/${cert_name}.cert.pem",
+      path    => '/usr/bin:/usr/sbin:/bin',
+      creates => $flagfile,
+    }
+      ~>
+    exec { 'dashboard.key':
+      command     => "ceph config-key set mgr/dashboard/key -i ${key_dir}/${cert_name}.key.pem",
+      path        => '/usr/bin:/usr/sbin:/bin',
+      refreshonly => true,
+    }
+      ~>
+    exec { 'reload dashboard':
+      command     => 'ceph mgr module disable dashboard && ceph mgr module enable dashboard',
+      path        => '/usr/bin:/usr/sbin:/bin',
+      refreshonly => true,
+    }
+      ~>
+    exec { 'dashboard-user':
+      command     => "ceph dashboard set-login-credentials ${dashboard_user} ${dashboard_password} && touch ${flagfile}",
+      path        => '/usr/bin:/usr/sbin:/bin',
+      refreshonly => true,
     }
   }
 }
