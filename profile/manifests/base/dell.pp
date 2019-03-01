9c9979fdc8cd2344312cdf130dc9f2c3092a6dae
Trond Hasle Amundsen <t.h.amundsen@usit.uio.no>
2019-03-01 15:43:32 +0100

    identify PowerEdge R7425

--- profile/manifests/base/dell.pp
+++ profile/manifests/base/dell.pp
@@ -13,7 +13,7 @@ class profile::base::dell (
   $manage_repos      = false,
   $manage_openmanage = false,
 ){
-  if fact('dmi.product.name') =~ '^PowerEdge (FC|[RTM])[1-9][1-4]0.*' {
+  if fact('dmi.product.name') =~ '^PowerEdge (FC|[RTM])[1-9][1-4]\d.*' {
 
     if $manage_repos {
 
