b86de72d37434c6eabb4f3c90212207645292ff5
TorLdre <tor.ledre@gmail.com>
2019-04-12 16:13:38 +0200

    enable iommu kernel param

--- profile/manifests/openstack/compute/pci_passthrough.pp
+++ profile/manifests/openstack/compute/pci_passthrough.pp
@@ -1,8 +1,18 @@
 #
 class profile::openstack::compute::pci_passthrough(
-  $manage_pci_alias =     false,
-  $manage_pci_whitelist = false,
+  $manage_pci_alias      = false,
+  $manage_pci_whitelist  = false,
+  $configure_intel_iommu = false,
+
 ) {
+
+  if $configure_intel_iommu {
+  	# Set option in grub2
+    kernel_parameter { 'intel_iommu':
+      ensure => present,
+      value  => 'on',
+    }
+  }
 
   if $manage_pcipassthrough {
     include ::nova::pci
