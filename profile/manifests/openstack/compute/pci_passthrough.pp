eece2f79ac177f9f0a2d2044bddb2887a5f96b4a
TorLdre <tor.ledre@gmail.com>
2019-04-12 12:40:39 +0200

    fix class

--- profile/manifests/openstack/compute/pci_passthrough.pp
+++ profile/manifests/openstack/compute/pci_passthrough.pp
@@ -3,6 +3,7 @@ class profile::openstack::compute::pci_passthrough(
   $manage_pcipassthrough = false,
 ) {
 
-if $manage_pcipassthrough {
-  include ::nova::pci
+  if $manage_pcipassthrough {
+    include ::nova::pci
+  }
 }
