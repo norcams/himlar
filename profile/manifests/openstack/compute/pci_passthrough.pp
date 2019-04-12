37d08e80fa110162cff0306128323bc3f86b5fec
TorLdre <tor.ledre@gmail.com>
2019-04-12 13:14:01 +0200

    enable pci whitelist, rename param

--- profile/manifests/openstack/compute/pci_passthrough.pp
+++ profile/manifests/openstack/compute/pci_passthrough.pp
@@ -1,9 +1,14 @@
 #
 class profile::openstack::compute::pci_passthrough(
-  $manage_pcipassthrough = false,
+  $manage_pci_alias =     false,
+  $manage_pci_whitelist = false,
 ) {
 
   if $manage_pcipassthrough {
     include ::nova::pci
+  }
+
+  if $manace_pci_whitelist {
+  	include ::nova::compute::pci
   }
 }
