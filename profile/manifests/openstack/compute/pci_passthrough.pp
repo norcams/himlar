17bb3b69fb4e041d9e59dd61fa542e60914fde4c
tanzr <tanzr@users.noreply.github.com>
2019-04-15 15:14:04 +0200

    is travis happy now?

--- profile/manifests/openstack/compute/pci_passthrough.pp
+++ profile/manifests/openstack/compute/pci_passthrough.pp
@@ -3,7 +3,7 @@ class profile::openstack::compute::pci_passthrough(
   $manage_pci_alias      = false,
   $manage_pci_whitelist  = false,
   $configure_intel_iommu = false,
-
+  $manage_pcipassthrough = false,
 ) {
 
   if $configure_intel_iommu {
@@ -15,10 +15,10 @@ class profile::openstack::compute::pci_passthrough(
   }
 
   if $manage_pcipassthrough {
-    include ::nova::pci,
+    include ::nova::pci
   }
 
   if $manage_pci_whitelist {
-    include ::nova::compute::pci,
+    include ::nova::compute::pci
   }
 }
