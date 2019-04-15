bbf3a89c063544722a6ac197250ecb8052adf462
tanzr <tanzr@users.noreply.github.com>
2019-04-15 14:59:53 +0200

    removed whitespace

--- profile/manifests/base/lvm.pp
+++ profile/manifests/base/lvm.pp
@@ -6,7 +6,6 @@ class profile::base::lvm (
 ){
   if $create_lvm {
     include ::lvm
-
     create_resources(physical_volume, lookup('profile::base::lvm::physical_volume', Hash, 'first', {}))
     create_resources(volume_group, lookup('profile::base::lvm::volume_group', Hash, 'first', {}))
     create_resources(lvm::logical_volume, lookup('profile::base::lvm::logical_volume', Hash, 'first', {}))
