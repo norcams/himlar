cc052d88a0be87e4540c0b49587a2f64c13ac0a2
Anders Vaage <anders@unix.uib.no>
2019-05-24 09:52:35 +0200

    do port configuration on opx with a post-up option in the interface configuration instead of a systemd job

--- profile/manifests/network/opx.pp
+++ profile/manifests/network/opx.pp
@@ -1,33 +1,6 @@
 class profile::network::opx(
-  $if_1gbit_force   = false,
   $opx_cleanup      = false,
 ) {
-
-  if $if_1gbit_force {
-    # Force to 1gbit without auto-negation
-    file {'/usr/local/bin/opx-ethtool-speed.sh':
-      ensure  => present,
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0755',
-      content => template("${module_name}/network/opx-ethtool-speed.erb")
-    } ->
-    file { '/etc/systemd/system/opx-ethtool-speed.service':
-      ensure  => present,
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0644',
-      seltype => 'systemd_unit_file_t',
-      content => "[Service]\r\nExecStart=/usr/local/bin/opx-ethtool-speed.sh\r\n",
-      notify  => [
-        Exec['systemctl_daemon_reload']
-      ],
-    } ->
-    exec { 'systemctl_daemon_reload':
-      command     => '/usr/bin/systemctl daemon-reload',
-      refreshonly => true,
-    }
-  }
 
   # What were they thinking...
   if $opx_cleanup {
