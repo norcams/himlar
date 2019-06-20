9cfb3d487ebc5dc391374ba50e24c9a9fc763fbe
TorLdre <tor.ledre@gmail.com>
2019-06-20 10:08:45 +0200

    add conditional

--- profile/manifests/openstack/network/calico.pp
+++ profile/manifests/openstack/network/calico.pp
@@ -4,6 +4,7 @@ class profile::openstack::network::calico(
   $manage_etcd               = false,
   $manage_etcd_grpc_proxy    = false,
   $manage_firewall           = true,
+  $manage_dhcp_agent         = false,
   $firewall_extras           = {},
 ) {
   include ::calico
@@ -41,35 +42,37 @@ class profile::openstack::network::calico(
 
   # Override ownership of the calico-dhcp-agent process as it should not be root
   # If calico-dhcp-agent was spawned as root, we must ensure correct permissions
-  file { 'calico-dhcp-agent-dir':
-    ensure  => directory,
-    path    => "/etc/systemd/system/calico-dhcp-agent.service.d",
-    owner   => root,
-    group   => root,
-  }
-  file { 'dhcp-agent-override':
-    ensure  => file,
-    path    => '/etc/systemd/system/calico-dhcp-agent.service.d/override.conf',
-    owner   => root,
-    group   => root,
-    content => "[Service]\nUser=neutron\n",
-    notify  => Service['calico-dhcp-agent']
-  }
-  file { 'neutron-log-perms':
-    ensure  => directory,
-    path    => "/var/log/neutron/",
-    owner   => neutron,
-    group   => neutron,
-    recurse => true,
-    notify  => Service['calico-dhcp-agent']
-  }
-  file { 'neutron-workdir-perms':
-    ensure  => directory,
-    path    => "/var/lib/neutron/dhcp/",
-    owner   => neutron,
-    group   => neutron,
-    recurse => true,
-    notify  => Service['calico-felix']
+  if $manage_dhcp_agent {
+    file { 'calico-dhcp-agent-dir':
+      ensure  => directory,
+      path    => "/etc/systemd/system/calico-dhcp-agent.service.d",
+      owner   => root,
+      group   => root,
+    }
+    file { 'dhcp-agent-override':
+      ensure  => file,
+      path    => '/etc/systemd/system/calico-dhcp-agent.service.d/override.conf',
+      owner   => root,
+      group   => root,
+      content => "[Service]\nUser=neutron\n",
+      notify  => Service['calico-dhcp-agent']
+    }
+    file { 'neutron-log-perms':
+      ensure  => directory,
+      path    => "/var/log/neutron/",
+      owner   => neutron,
+      group   => neutron,
+      recurse => true,
+      notify  => Service['calico-dhcp-agent']
+    }
+    file { 'neutron-workdir-perms':
+      ensure  => directory,
+      path    => "/var/lib/neutron/dhcp/",
+      owner   => neutron,
+      group   => neutron,
+      recurse => true,
+      notify  => Service['calico-felix']
+    }
   }
 
   if $manage_firewall {
