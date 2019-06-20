62f0b76b2bdee1b17041c3dcc7000af9c784703f
Anders Vaage <anders@unix.uib.no>
2019-06-20 12:41:24 +0200

    add missing closing bracket and fix linting at the same time

--- profile/manifests/base/common.pp
+++ profile/manifests/base/common.pp
@@ -109,18 +109,18 @@ class profile::base::common (
   }
 
   if $extraswap {
-    exec { "create_swapfile":
-      command     => "dd if=/dev/zero of=${extraswap_fileloc} bs=1M count=$((${extraswap_sizegb}*1024)) && sudo chmod 600 ${extraswap_fileloc}",
-      path        => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
-      unless      => ["test -f ${extraswap_fileloc}"],
-      require     => Class['profile::base::lvm'],
+    exec { 'create_swapfile':
+      command => "dd if=/dev/zero of=${extraswap_fileloc} bs=1M count=$((${extraswap_sizegb}*1024)) && sudo chmod 600 ${extraswap_fileloc}",
+      path    => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+      unless  => ["test -f ${extraswap_fileloc}"],
+      require => Class['profile::base::lvm'],
     } ~>
-    exec { "activate_extraswap":
+    exec { 'activate_extraswap':
       command     => "mkswap ${extraswap_fileloc} && swapon ${extraswap_fileloc}",
       path        => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
       refreshonly => true,
     } ~>
-    exec { "add_extraswap_to_fstab":
+    exec { 'add_extraswap_to_fstab':
       command     => "echo \"${extraswap_fileloc} swap swap defaults 0 0\n\" >> /etc/fstab",
       path        => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
       refreshonly => true,
@@ -133,8 +133,9 @@ class profile::base::common (
   }
 
   if $manage_vm_swappiness {
-    sysctl::value { "vm.swappiness":
+    sysctl::value { 'vm.swappiness':
       value => $vm_swappiness,
+    }
   }
 
   if $manage_sysctl {
