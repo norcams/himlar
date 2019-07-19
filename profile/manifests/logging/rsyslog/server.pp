080417580aaafbe00b9ca4338055133e0be21811
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2019-07-19 13:52:32 +0200

    Updated rsyslog on logger

--- profile/manifests/logging/rsyslog/server.pp
+++ profile/manifests/logging/rsyslog/server.pp
@@ -1,11 +1,18 @@
 # rsyslog server
 class profile::logging::rsyslog::server(
   $manage_firewall = true,
+  $manage_sysconfig = false,
+  $domains = lookup('domain_mgmt', String, 'first'),
   $firewall_extras = {},
 ) {
 
   include ::rsyslog::server
 
+  if $manage_sysconfig {
+    file { '/etc/sysconfig/rsyslog':
+      content => template("${module_name}/logging/rsyslog/default.erb")
+    }
+  }
   if $manage_firewall {
 
     profile::firewall::rule { '150 rsyslog accept udp':
