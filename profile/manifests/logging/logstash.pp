c338722e97edb4c9bac6c366702969133db33d2f
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2019-07-19 14:55:29 +0200

    Updated logstash config

--- profile/manifests/logging/logstash.pp
+++ profile/manifests/logging/logstash.pp
@@ -1,11 +1,12 @@
 #
-class profile::logging::logstash() {
+class profile::logging::logstash(
+  $config_files = {}
+) {
 
   include ::logstash
 
-  ::logstash::configfile { 'openstack':
-    content => template("${module_name}/logging/logstash/openstack.conf.erb")
-  }
+  # This is the config files in /etc/logstash/conf.d/
+  create_resources('logstash::configfile', $config_files)
 
   file { '/etc/logstash/patterns/patterns':
     content => template("${module_name}/logging/logstash/patterns.erb")
