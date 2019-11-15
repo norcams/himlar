e89e4f368d4da6682be87fb68001927531e09d64
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2019-11-15 14:01:41 +0100

    Updated collectd for cephmon

--- profile/manifests/monitoring/collectd.pp
+++ profile/manifests/monitoring/collectd.pp
@@ -8,10 +8,26 @@ class profile::monitoring::collectd(
 
   if $enable {
     include ::collectd
+    include ::profile::base::systemd
 
     # plugins
     $plugins  = lookup('profile::monitoring::collectd::plugins', Array, $merge_strategy, [])
     $plugins.include
+
+    file { 'collectd-systemd-dir':
+      ensure => directory,
+      path   => '/etc/systemd/system/collectd.service.d/',
+      owner  => root,
+      group  => root,
+    }
+    -> file { 'collectd-systemd-override':
+      ensure  => file,
+      path    => '/etc/systemd/system/collectd.service.d/override.conf',
+      owner   => root,
+      group   => root,
+      content => "[Service]\nCapabilityBoundingSet=CAP_DAC_OVERRIDE\n",
+      notify  => [Exec['systemctl_daemon_reload'], Service['collectd']]
+    }
 
   }
 
