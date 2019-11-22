37eec142b0b3447fc10708069622349ec3014d5d
Anders Vaage <anders@unix.uib.no>
2019-11-22 14:37:07 +0100

    move ceph command output parsing to a function in ceph_osd custom fact, wrap ceph command execution in setcode block to make confine work

--- profile/lib/facter/ceph_osd.rb
+++ profile/lib/facter/ceph_osd.rb
@@ -1,11 +1,7 @@
 require 'json'
 
-Facter.add('ceph_osds') do
-  confine hostname: /storage|object-ceph/
-
-  output = Facter::Core::Execution.execute('ceph osd tree -f json',
-                                           timeout: 30)
-  osds = []
+def parse_output(output)
+  osd_array = []
   parsed_output = JSON.parse(output)['nodes']
   parsed_output.each do |node|
     next unless node['name'] == Facter.value(:hostname)
@@ -13,7 +9,16 @@ Facter.add('ceph_osds') do
       osds << "ceph-osd.#{osd}"
     end
   end
+  osd_array
+end
+
+Facter.add('ceph_osds') do
+  confine hostname: /storage|object-ceph/
+
   setcode do
+    output = Facter::Core::Execution.execute('ceph osd tree -f json',
+                                             timeout: 30)
+    osds = parse_output(output)
     osds
   end
 end
