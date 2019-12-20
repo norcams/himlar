1327043ff12a6d6539e2041b6ab75310bad2006e
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2019-12-20 12:04:55 +0100

    Drop kvm-intel on amd compute nodes

--- profile/manifests/virtualization/nested.pp
+++ profile/manifests/virtualization/nested.pp
@@ -5,17 +5,20 @@
 # http://docs.openstack.org/developer/devstack/guides/devstack-with-nested-kvm.html
 #
 class profile::virtualization::nested(
+  $manage = false,
   $module = 'kvm-intel',
   $option = 'nested',
   $value = 'y'
 ) {
 
-  include ::kmod
-  kmod::load { $module: }
+  if $manage {
+    include ::kmod
+    kmod::load { $module: }
 
-  kmod::option { "Turn on nested virtualization":
-        module => $module,
-        option => $option,
-        value =>  $value
-      }
+    kmod::option { 'Turn on nested virtualization':
+      module => $module,
+      option => $option,
+      value  =>  $value
+    }
+  }
 }
