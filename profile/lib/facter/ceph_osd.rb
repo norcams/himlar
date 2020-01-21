a87e432b5932cac81ea9cbb9637323794bfa2aaa
Anders Vaage <anders@unix.uib.no>
2020-01-21 14:12:04 +0100

    this should work

--- profile/lib/facter/ceph_osd.rb
+++ profile/lib/facter/ceph_osd.rb
@@ -2,11 +2,13 @@ require 'json'
 
 def parse_output(output)
   osd_array = []
-  parsed_output = JSON.parse(output)['nodes']
-  parsed_output.each do |node|
-    next unless node['name'] == Facter.value(:hostname)
-    node['children'].each do |osd|
-      osd_array << "ceph-osd.#{osd}"
+  if output.kind_of?(Array)
+    parsed_output = JSON.parse(output)['nodes']
+    parsed_output.each do |node|
+      next unless node['name'] == Facter.value(:hostname)
+      node['children'].each do |osd|
+        osd_array << "ceph-osd.#{osd}"
+      end
     end
   end
   osd_array
@@ -18,9 +20,7 @@ Facter.add('ceph_osds') do
   setcode do
     output = Facter::Core::Execution.execute('which ceph && ceph osd tree -f json 2>/dev/null',
                                              timeout: 30)
-    unless output.empty?
-      osds = parse_output(output)
-      osds
-    end
+    osds = parse_output(output)
+    osds
   end
 end
