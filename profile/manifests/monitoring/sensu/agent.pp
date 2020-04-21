843e2df3f561056488d53298dbdab5e6d6b0b0ff
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2020-04-21 12:55:35 +0200

    Added sensu client to torack role

--- profile/manifests/monitoring/sensu/agent.pp
+++ profile/manifests/monitoring/sensu/agent.pp
@@ -1,14 +1,15 @@
 #
 class profile::monitoring::sensu::agent (
   $enable_agent = false,
+  String $merge_strategy = 'deep'
 ) {
 
   if $enable_agent {
     include ::sensu
 
-    $gems = lookup('profile::monitoring::sensu::agent::plugin_gems', Hash, 'deep', {})
-    $plugins = lookup('profile::monitoring::sensu::agent::plugins', Hash, 'deep', {})
-    $checks  = lookup('profile::monitoring::sensu::agent::checks', Hash, 'deep', {})
+    $gems = lookup('profile::monitoring::sensu::agent::plugin_gems', Hash, $merge_strategy, {})
+    $plugins = lookup('profile::monitoring::sensu::agent::plugins', Hash, $merge_strategy, {})
+    $checks  = lookup('profile::monitoring::sensu::agent::checks', Hash, $merge_strategy, {})
 
     create_resources('::sensu::plugin', $plugins)
     create_resources('::sensu::check', $checks)
