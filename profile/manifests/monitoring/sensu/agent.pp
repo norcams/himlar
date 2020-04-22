640ab2ee23e783f04fe68b031ebad66c63c6b0db
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2020-04-22 11:28:04 +0200

    Run sensu-client in mgmt vrf in torack

--- profile/manifests/monitoring/sensu/agent.pp
+++ profile/manifests/monitoring/sensu/agent.pp
@@ -1,6 +1,7 @@
 #
 class profile::monitoring::sensu::agent (
   $enable_agent = false,
+  Boolean $run_in_vrf = false,
   String $merge_strategy = 'deep'
 ) {
 
@@ -14,6 +15,17 @@ class profile::monitoring::sensu::agent (
     create_resources('::sensu::plugin', $plugins)
     create_resources('::sensu::check', $checks)
     create_resources('package', $gems)
+
+    if $run_in_vrf {
+      file { 'sensu-systemd-override':
+        ensure => file,
+        path   => '/etc/systemd/system/sensu-client.service.d/override.conf',
+        owner  => root,
+        group  => root,
+        source => "puppet:///modules/${module_name}/monitoring/sensu/systemd/override.conf",
+        notify => [Exec['systemctl_daemon_reload'], Service['sensu-client']]
+      }
+    }
 
   }
 }
