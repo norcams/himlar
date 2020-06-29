75ffec553aec934448b4ec3bf377ebd792babcd3
TorLdre <tor.ledre@gmail.com>
2020-06-29 14:35:27 +0200

    change mech for creation and params for replicated pools

--- profile/manifests/storage/cephpool.pp
+++ profile/manifests/storage/cephpool.pp
@@ -4,13 +4,18 @@
 class profile::storage::cephpool (
   $manage_ec_pools        = false,
   $manage_cephpool_params = false,
+  $manage_repl_pools      = false,
 ) {
   require ::ceph::profile::client
 
-  create_resources(ceph::pool, lookup('profile::storage::cephpool::pools', Hash, 'first', {}))
+  if $manage_repl_pools {
+    create_resources(ceph::pool, lookup('profile::storage::cephpool::pools', Hash, 'first', {}))
+  }
+
   if $manage_ec_pools {
     create_resources(profile::storage::ceph_ecpool, lookup('profile::storage::ceph_ecpool::ec_pools', Hash, 'first'))
   }
+
   if $manage_cephpool_params {
     create_resources(profile::storage::ceph_crushrules, lookup('profile::storage::ceph_crushrules::rules', Hash, 'first'))
     create_resources(profile::storage::cephpool_params, lookup('profile::storage::cephpool_params::pools', Hash, 'first'))
