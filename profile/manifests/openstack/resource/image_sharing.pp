0829f57b01cf70d006b73dbf3041c17e0d3cb78a
Trond Hasle Amundsen <t.h.amundsen@usit.uio.no>
2020-07-03 10:44:01 +0200

    add profile code to create project for image sharing

---
+++ profile/manifests/openstack/resource/image_sharing.pp
@@ -0,0 +1,17 @@
+# Manage project for sharing images
+class profile::openstack::resource::image_sharing(
+  $manage  = false,
+  $ensure  = present,
+  $project = 'nrec-images'
+  $domain  = 'dataporten'
+) {
+
+  if $manage {
+    keystone_tenant { $project::$domain:
+      ensure      => $ensure,
+      enabled     => true,
+      description => "Project for sharing images",
+    }
+  }
+
+}
