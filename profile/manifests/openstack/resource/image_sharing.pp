d5800fae513a996d9d1a399ecb315e80fcc0658a
Trond Hasle Amundsen <t.h.amundsen@usit.uio.no>
2020-07-03 10:54:35 +0200

    fix git-amending failure

--- profile/manifests/openstack/resource/image_sharing.pp
+++ profile/manifests/openstack/resource/image_sharing.pp
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
 # Manage project for sharing images
-=======
-# Manage project user for sharing images
->>>>>>> 69b427ded23622675b7e92dd417c03bbc7e91e1c
 class profile::openstack::resource::image_sharing(
   $manage  = false,
   $ensure  = present,
   $project = 'nrec-images'
-<<<<<<< HEAD
   $domain  = 'dataporten'
-=======
-  $domain  = 'default'
->>>>>>> 69b427ded23622675b7e92dd417c03bbc7e91e1c
 ) {
 
   if $manage {
