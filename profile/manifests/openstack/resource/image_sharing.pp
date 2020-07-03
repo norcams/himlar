245a58691d0f8a5d9e05021b9332cd9a934ce966
Trond Hasle Amundsen <t.h.amundsen@usit.uio.no>
2020-07-03 10:54:58 +0200

    bugfix

--- profile/manifests/openstack/resource/image_sharing.pp
+++ profile/manifests/openstack/resource/image_sharing.pp
@@ -2,7 +2,7 @@
 class profile::openstack::resource::image_sharing(
   $manage  = false,
   $ensure  = present,
-  $project = 'nrec-images'
+  $project = 'nrec-images',
   $domain  = 'dataporten'
 ) {
 
