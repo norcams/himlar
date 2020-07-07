ad3a1234d83b8a1f5bb3b85028e23e91043ead73
Trond Hasle Amundsen <t.h.amundsen@usit.uio.no>
2020-07-07 13:11:01 +0200

    foo

--- profile/manifests/rhsm/virtwho.pp
+++ profile/manifests/rhsm/virtwho.pp
@@ -1,14 +1,15 @@
 class profile::rhsm::virtwho (
-  $manage                  = false,
-  $owner                   = undef,
-  $rhsm_hostname           = undef,
-  $rhsm_username           = undef,
-  $rhsm_encrypted_password = undef,
-  $rhsm_proxy_hostname     = '172.28.32.12',
-  $rhsm_proxy_port         = '8888'
+  $manage              = false,
+  $rhsm_username       = undef,
+  $rhsm_password       = undef,
+  $rhsm_proxy_hostname = '172.28.32.12',
+  $rhsm_proxy_port     = '8888'
 ) {
 
   if $manage {
+
+    $owner         = $::profile::rhsm::subscription::organization
+    $rhsm_hostname = $::profile::rhsm::subscription::server
 
     package { 'virt-who':
       ensure => installed,
