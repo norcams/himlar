1dcf18ee031b03a4f69a7881cab79cc2fb260a37
Trond Hasle Amundsen <t.h.amundsen@usit.uio.no>
2020-07-10 12:27:22 +0200

    foo

--- profile/manifests/rhsm/subscription.pp
+++ profile/manifests/rhsm/subscription.pp
@@ -16,6 +16,9 @@ class profile::rhsm::subscription (
       service_name    => 'rhsmcertd',
       force           => true,
     }
-    
+
+    exec { "disable_subscription_manager_yum_plugin":
+      command => "/usr/bin/sed -i -e 's/enabled=1/enabled=0/' /etc/yum/pluginconf.d/subscription-manager.conf",
+      unless  => "/usr/bin/grep -q 'enabled=0' /etc/yum/pluginconf.d/subscription-manager.conf"
   }
 }
