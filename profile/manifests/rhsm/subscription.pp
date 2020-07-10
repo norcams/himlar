6d91df38bc089a3a276241cbc3b161293e37aad3
Trond Hasle Amundsen <t.h.amundsen@usit.uio.no>
2020-07-10 10:58:48 +0200

    foo

--- profile/manifests/rhsm/subscription.pp
+++ profile/manifests/rhsm/subscription.pp
@@ -10,9 +10,9 @@ class profile::rhsm::subscription (
     $activationkey = lookup('uio_satellite_activationkey', String, 'first', '')
 
     rhsm_register { "$server":
-      server_hostname => $server,
-      org             => $organization,
-      activationkey   => $activationkey,
+      server_hostname => "$server",
+      org             => "$organization",
+      activationkey   => "$activationkey",
       autosubscribe   => false,
       force           => false,
     }
