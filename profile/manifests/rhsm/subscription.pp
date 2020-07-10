97c78f6374bd4fffe7f1087c89aa4575e337d967
Trond Hasle Amundsen <t.h.amundsen@usit.uio.no>
2020-07-10 11:18:59 +0200

    foo

--- profile/manifests/rhsm/subscription.pp
+++ profile/manifests/rhsm/subscription.pp
@@ -3,18 +3,17 @@ class profile::rhsm::subscription (
 ) {
 
   if $manage {
-    include ::subscription_manager
 
     $server        = lookup('uio_satellite_server', String, 'first', '')
     $organization  = lookup('uio_satellite_organization', String, 'first', '')
     $activationkey = lookup('uio_satellite_activationkey', String, 'first', '')
 
-    rhsm_config { "$server" }
-
-    rhsm_register { "$server":
-      org             => "$organization",
-      activationkey   => "$activationkey",
+    class { 'subscription_manager':
+      server_hostname => $server,
+      org             => $organization,
+      activationkey   => $activationkey,
       autosubscribe   => false,
+      service_name    => 'rhsmcertd',
       force           => false,
     }
 
