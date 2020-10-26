ac1daa5641072c114cfe8839c57ac2cd08dbe33f
tanzr <tanzr@users.noreply.github.com>
2020-10-26 16:50:32 +0100

    clear old caches

--- profile/manifests/logging/logrotate.pp
+++ profile/manifests/logging/logrotate.pp
@@ -1,9 +1,21 @@
 #
 class profile::logging::logrotate(
-  $manage_logrotate = false,
+  $manage_logrotate 		= false,
+  $manage_monitor_logrotate = false,
+
 ) {
 
   if $manage_logrotate {
     include ::logrotate
   }
+
+  if $manage_monitor_logrotate {
+    tidy { 'carbon-cache':
+      path  =>  '/opt/graphite/storage/log/carbon-cache/carbon-cache-a/',
+      age => '24w',
+      recurse => true,
+      matches => '.*log.*',
+    }
+  }
+
 }
