e21c30f4712dee145d77b1ec09d9e73b580c5720
edpto <tor.ladre@it.uib.no>
2021-08-24 12:48:26 +0200

    use new provider for el8 installs

--- profile/manifests/base/yumrepo.pp
+++ profile/manifests/base/yumrepo.pp
@@ -1,15 +1,25 @@
 # Manages repo with https://github.com/openstack/puppet-openstack_extras
+# or the new yumrepo_core depending on el version
 # This class is part of profile::base::common
 class profile::base::yumrepo(
   $merge_strategy = 'deep'
 ) {
 
-  $repo_hash = lookup('profile::base::yumrepo::repo_hash', Hash, $merge_strategy, {})
+$repo_hash = lookup('profile::base::yumrepo::repo_hash', Hash, $merge_strategy, {})
 
-  # Openstack_extras uses yumrepo resource from core puppet for repo_hash syntax:
-  # https://docs.puppet.com/puppet/3.8/reference/types/yumrepo.html
-  class { '::openstack_extras::repo::redhat::redhat':
-    repo_hash => $repo_hash
+  case $::operatingsystemmajrelease {
+    '7': {
+      # Openstack_extras uses yumrepo resource from core puppet for repo_hash syntax:
+      # https://docs.puppet.com/puppet/3.8/reference/types/yumrepo.html
+      class { '::openstack_extras::repo::redhat::redhat':
+        repo_hash => $repo_hash
+      }
+    }
+    # From puppet 6 we use the new yumrepo_core from puppetlabs
+    '8': {
+      create_resources('yumrepo_core::yumrepo', $repo_hash)
+    }
+    default: {
+    }
   }
-
 }
