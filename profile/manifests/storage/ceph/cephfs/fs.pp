a83ddfdbfac5964494931c0ed00b0b211997453f
edpto <tor.ladre@it.uib.no>
2021-09-09 13:48:48 +0200

    fix travis complaints

--- profile/manifests/storage/ceph/cephfs/fs.pp
+++ profile/manifests/storage/ceph/cephfs/fs.pp
@@ -7,6 +7,6 @@ define profile::storage::ceph::cephfs::fs(
   exec { "cepfs_create_${name}":
     command => "ceph fs new ${name} ${metadata_pool} ${primary_data_pool}",
     path    => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
-    unless  => "ceph fs ls | gawk '{ print $2 }' | grep -w ${name}"
+    unless  => "ceph fs ls | gawk '{ print \$2 }' | grep -w ${name}"
   }
 }
