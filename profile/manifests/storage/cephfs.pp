e483e5a332366f4a5a4a471d74fbe9fc12818919
edpto <tor.ladre@it.uib.no>
2021-09-09 13:01:15 +0200

    mds can mount and create cephfs

--- profile/manifests/storage/cephfs.pp
+++ profile/manifests/storage/cephfs.pp
@@ -3,6 +3,7 @@
 #
 class profile::storage::cephfs(
   $create_cephfs = false,
+  $mount_cephfs  = false,
 ) {
 
   require ::ceph::profile::client
@@ -10,5 +11,9 @@ class profile::storage::cephfs(
   if $create_cephfs {
     $filesystems = lookup('profile::storage::cephfs::filesystems', Hash, 'deep', {})
     create_resources('profile::storage::ceph::cephfs::fs', $filesystems)
+  }
+  if $mount_cephfs {
+    $mount_filesystems = lookup('profile::storage::cephfs::mount', Hash, 'deep', {})
+    create_resources('profile::storage::ceph::cephfs::mount', $mount_filesystems)
   }
 }
