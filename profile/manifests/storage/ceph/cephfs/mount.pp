258f62b3d169aa9099eec3b2c7c3263e3cba379d
edpto <tor.ladre@it.uib.no>
2021-09-09 12:38:14 +0200

    add mech for mounting cephfs with kernel module

---
+++ profile/manifests/storage/ceph/cephfs/mount.pp
@@ -0,0 +1,31 @@
+# Mount cephfs filesystem
+define profile::storage::ceph::cephfs::mount(
+  $ensure               = present,          # set to mounted to actually mount the file system
+  $fstype               = 'ceph',
+  $local_mountdir_owner = 'root',
+  $device               = ':/',             # Exmaples: :/ecdata or :/ to mount a subdir in cephfs or the cephfs root
+  $user                 = 'admin',          # Client keys must exist in /etc/ceph
+  $mount_options        = '_netdev,noauto', # String of generic mount options as they appear in fstab, should always contain _netdev
+) {
+
+  exec { "create_mountdir_${name}":
+    command => "mkdir -p ${local_mountdir}",
+    path    => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+    unless  => "test -d ${local_mountdir}",
+  } ~>
+  file { "local_mountdir_${name}":
+    path    => "${name}",
+    ensure  => directory,
+    owner   => "${local_mountdir_owner}",
+    recurse => true,
+  }
+
+  $mount_opts = "name=${user},${mount_options}"
+
+  mount { "${name}":
+    ensure  => "${ensure}",
+    fstype  => "${fstype}",
+    device  => "${device}",
+    options => "${mount_opts}",
+  }
+}
