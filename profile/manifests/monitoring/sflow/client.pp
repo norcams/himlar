ae21b6fe0e684b49798e380c8c2cf6db817fa360
TorLdre <tor.ledre@gmail.com>
2021-11-04 15:25:43 +0100

    add sflow collector role and hsflowd client

---
+++ profile/manifests/monitoring/sflow/client.pp
@@ -0,0 +1,20 @@
+#
+class profile::monitoring::sflow::client (
+  $manage_service        = false,
+  $hsflowd_template      = 'hsflowd_spine.conf.erb',
+) {
+
+  file { 'hsflowd.conf':
+    ensure  => file,
+    path    => '/etc/hsflowd.conf',
+    content => template("${module_name}/monitoring/sflow/${hsflowd_template}"),
+    mode    => '0644',
+  }
+
+  if $manage_service {
+    service { 'hsflowd':
+      ensure => running,
+      enable => true
+    }
+  }
+}
--- profile/manifests/monitoring/sflow/server.pp
+++ profile/manifests/monitoring/sflow/server.pp
@@ -1,7 +1,26 @@
 #
 class profile::monitoring::sflow::server (
-  $manage_sflow2graphite     = false,
+  $manage_sflow2graphite = false,
+  $manage_service        = false,
+  $manage_firewall       = false,
+  $firewall_extras       = undef,
 ) {
+
+  if $manage_service {
+    service { 'sflow-rt':
+      ensure => running,
+      enable => true
+    }
+  }
+
+  if $manage_firewall {
+    profile::firewall::rule { '314 sflow accept udp':
+      proto   => udp,
+      dport   => 6343,
+      iniface => $::ipaddress_trp1,
+      extras  => $firewall_extras,
+    }
+  }
 
   # Install tools for converting sflow data
   if $manage_sflow2graphite {
