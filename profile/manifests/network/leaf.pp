1653e4fdb82a23c8d3ea79b0a2a069bc6acfbbe4
edpto <tor.ladre@it.uib.no>
2021-11-25 12:46:12 +0100

    enable config for nodes

--- profile/manifests/network/leaf.pp
+++ profile/manifests/network/leaf.pp
@@ -6,7 +6,7 @@ class profile::network::leaf(
   $manage_quagga      = false,
   $manage_frrouting   = false,
   $manage_acls        = false,
-  $manage_switchd     = false,
+  $manage_switchdconf = false,
   $manage_portconfig  = false,
   $cumulus_portconfig = {},
   $switchd_config     = {},
@@ -32,5 +32,8 @@ class profile::network::leaf(
   }
   if $manage_portconfig {
     create_resources(profile::network::leaf::cumulus_portconfig, lookup('profile::network::leaf::cumulus_portconfigs', Hash, 'deep', {}))
+  }
+  if $manage_switchdconf {
+    create_resources(profile::network::leaf::switchd_config, lookup('profile::network::leaf::switchdconf', Hash, 'deep', {}))
   }
 }
