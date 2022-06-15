91aa1cfbe6c10e33f0ea0f7878a0d0d377ae22b0
TorLdre <tor.ledre@gmail.com>
2022-06-15 18:57:26 +0200

    rebuild initramfs when forcing disk driver load order

--- profile/manifests/base/physical.pp
+++ profile/manifests/base/physical.pp
@@ -82,6 +82,12 @@ class profile::base::physical (
     file { "/etc/modprobe.d/${load_ahci_first_scsidrv}.conf":
       ensure  => present,
       content => "install ${load_ahci_first_scsidrv} /sbin/modprobe ahci; /sbin/modprobe --ignore-install ${load_ahci_first_scsidrv}\n",
+      notify  => Exec['rebuild initramfs_first']
+    }
+    exec { 'rebuild initramfs_first':
+      command     => 'dracut -f --kver $(rpm -qa kernel | sort -V -r | head -n 1 | sed \'s|kernel-||\')',
+      path        => '/sbin:/usr/bin:/usr/sbin',
+      refreshonly => true
     }
   }
 
@@ -90,6 +96,12 @@ class profile::base::physical (
     file { "/etc/modprobe.d/ahci.conf":
       ensure  => present,
       content => "install ahci /sbin/modprobe ${load_ahci_last_scsidrv}; /sbin/modprobe --ignore-install ahci\n",
+      notify  => Exec['rebuild initramfs_last']
+    }
+    exec { 'rebuild initramfs_last':
+      command     => 'dracut -f --kver $(rpm -qa kernel | sort -V -r | head -n 1 | sed \'s|kernel-||\')',
+      path        => '/sbin:/usr/bin:/usr/sbin',
+      refreshonly => true
     }
   }
 
