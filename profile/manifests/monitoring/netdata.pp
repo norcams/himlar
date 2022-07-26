d50372147a7c995178222f24766ba78f7947c824
Raymond Kristiansen <raymond.kristiansen@uib.no>
2022-07-26 08:41:18 +0200

    feat: make netdata firewall rules optinal and default turned off

--- profile/manifests/monitoring/netdata.pp
+++ profile/manifests/monitoring/netdata.pp
@@ -1,19 +1,21 @@
 #
 class profile::monitoring::netdata(
+  $enable_netdata            = false,
   $manage_firewall           = false,
   $firewall_dest             = $::ipaddress_mgmt1,
   $firewall_source           = $::ipaddress_mgmt1,
 ) {
 
-  include ::netdata
+  if $enable_netdata {
+    include ::netdata
 
-  if $manage_firewall {
-    profile::firewall::rule { '466 netdata accept tcp':
-      dport       => 19999,
-      proto       => 'tcp',
-      destination => $firewall_dest,
-      source      => $firewall_source,
+    if $manage_firewall {
+      profile::firewall::rule { '466 netdata accept tcp':
+        dport       => 19999,
+        proto       => 'tcp',
+        destination => $firewall_dest,
+        source      => $firewall_source,
+      }
     }
   }
-
 }
