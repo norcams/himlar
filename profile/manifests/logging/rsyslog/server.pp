3cb246b39fa982405fb3084e4221f825b9bb5f60
Micke <mikaeld66@users.noreply.github.com>
2022-08-04 12:38:47 +0200

    Update rsyslog module (#794)
    Co-authored-by: Mikael Dalsgard <mikaeld@uio.no>

--- profile/manifests/logging/rsyslog/server.pp
+++ profile/manifests/logging/rsyslog/server.pp
@@ -5,9 +5,22 @@ class profile::logging::rsyslog::server(
   $manage_log_dir = false,
   $domains = lookup('domain_mgmt', String, 'first'),
   $firewall_extras = {},
+  $modules         = {},
+  $global_config   = {},
+  $legacy_config   = {},
+  $inputs          = {},
+  $rulesets        = {},
+  $templates       = {},
 ) {
 
-  include ::rsyslog::server
+  class { 'rsyslog::config' :
+    modules       => $modules,
+    global_config => $global_config,
+    legacy_config => $legacy_config,
+    inputs        => $inputs,
+    rulesets      => $rulesets,
+    templates     => $templates,
+  }
 
   if $manage_sysconfig {
     file { '/etc/sysconfig/rsyslog':
