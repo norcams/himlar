16684eca3b5fbdd358caeb31473ced443d039951
Raymond Kristiansen <raymond.kristiansen@uib.no>
2022-12-06 11:20:56 +0100

    feat: added enable/disable option to power metric

--- profile/manifests/monitoring/physical/power.pp
+++ profile/manifests/monitoring/physical/power.pp
@@ -4,19 +4,23 @@
 class profile::monitoring::physical::power(
   String $http_proxy,
   String $statsd_server,
-  String $statsd_port = 8125
+  Boolean $enable = false,
+  String $statsd_port = '8125'
 ) {
-  $bmc_username = lookup('bmc_username', String, 'first', '')
-  $bmc_password = lookup("bmc_password_${::location}", String, 'first', '')
 
-  $bmc_network  = regsubst($::ipaddress_trp1, '^(\d+)\.(\d+)\.(\d+)\.(\d+)$','\2',) - 1
-  $bmc_address  = regsubst($::ipaddress_trp1, '^(\d+)\.(\d+)\.(\d+)\.(\d+)$',"\\1.${bmc_network}.\\3.\\4",)
+  if $enable {
+    $bmc_username = lookup('bmc_username', String, 'first', '')
+    $bmc_password = lookup("bmc_password_${::location}", String, 'first', '')
 
-  file { 'power_metric.sh':
-    ensure  => file,
-    path    => '/usr/local/bin/power_metric.sh',
-    content => template("${module_name}/monitoring/sensu/power_metric.sh.erb"),
-    mode    => '0755',
+    $bmc_network  = regsubst($::ipaddress_trp1, '^(\d+)\.(\d+)\.(\d+)\.(\d+)$','\2',) - 1
+    $bmc_address  = regsubst($::ipaddress_trp1, '^(\d+)\.(\d+)\.(\d+)\.(\d+)$',"\\1.${bmc_network}.\\3.\\4",)
+
+    file { 'power_metric.sh':
+      ensure  => file,
+      path    => '/usr/local/bin/power_metric.sh',
+      content => template("${module_name}/monitoring/sensu/power_metric.sh.erb"),
+      mode    => '0755',
+    }
   }
 
 }
