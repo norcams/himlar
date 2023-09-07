83701691c5a550f37bb22ca3ba0fa17d7db0ffe0
Raymond Kristiansen <raymond.kristiansen@uib.no>
2023-09-07 14:16:05 +0200

    refactor: change logrotate for syslog on different platforms and roles (#854)

--- profile/manifests/logging/logrotate.pp
+++ profile/manifests/logging/logrotate.pp
@@ -1,9 +1,17 @@
 #
 class profile::logging::logrotate(
-  $manage_logrotate = false,
+  Boolean $manage_logrotate = false,
+  String $merge_strategy = 'deep',
 ) {
 
   if $manage_logrotate {
     include ::logrotate
+
+    # This will apply default rules usually defined in platform
+    # Use logrotate::rules in roles to add more rules or
+    # set $merge_strategy = first and create a new complete
+    # profile::logging::logrotate::rules entry in roles
+    $rules  = lookup('profile::logging::logrotate::rules', Hash, $merge_strategy, {})
+    create_resources('logrotate::rule', $rules)
   }
 }
