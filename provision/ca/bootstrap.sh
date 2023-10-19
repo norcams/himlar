203d99e29cd553188df41cc0b5280c0dbe989b9e
Raymond Kristiansen <raymond.kristiansen@uib.no>
2023-10-19 08:47:00 +0200

    fix(vagrant): minor cleanup for vagrant CA

--- provision/ca/bootstrap.sh
+++ provision/ca/bootstrap.sh
@@ -10,10 +10,11 @@ chmod 700 private
 touch index.txt
 echo 1000 > serial
 
-
+# Create CA key
 openssl genrsa -aes256 -passout file:passfile -out private/ca.key.pem 4096
 chmod 400 private/ca.key.pem
 
+# Create CA cert
 openssl req -config openssl.cnf \
       -key private/ca.key.pem -passin file:passfile -batch \
       -new -x509 -days 7300 -sha256 -extensions v3_ca \
