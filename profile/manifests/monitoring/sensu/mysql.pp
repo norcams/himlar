b2f8b51afab619e43a3fa88fd4a9961e626f819b
Raymond Kristiansen <raymond.kristiansen@uib.no>
2023-11-10 09:16:11 +0100

    feat(vagrant): minor changes to make sensu-go work

--- profile/manifests/monitoring/sensu/mysql.pp
+++ profile/manifests/monitoring/sensu/mysql.pp
@@ -28,4 +28,25 @@ class profile::monitoring::sensu::mysql(
   } else {
     info('sensu agent disabled for mysql')
   }
+
+  # Do nothing if agent is not enabled
+  $enable_go_agent = lookup('profile::monitoring::sensu::agent::enable_go_agent', Boolean, 'first', false)
+
+  if $enable_go_agent {
+    mysql_user { "${user}@${host}":
+      ensure                   => 'present',
+      password_hash            => mysql_password($password),
+      max_connections_per_hour => '0',
+      max_queries_per_hour     => '0',
+    }
+
+    file { '/etc/sensu/mysql.ini':
+      ensure  => file,
+      content => template("${module_name}/monitoring/sensu/mysql.ini.erb"),
+      require => Class['profile::monitoring::sensu::agent']
+    }
+
+  } else {
+    info('sensu agent disabled for mysql')
+  }
 }
