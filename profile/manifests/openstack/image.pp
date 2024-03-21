b0128b12cf124fa4f19fc3319e03bf1f615b2529
Ivar Thokle Hovden <ivarth@uio.no>
2024-03-21 10:29:17 +0100

    feat(vagrant): enable OSProfiler for nova, cinder, glance, neutron and keystone with redis collector on proxy

--- profile/manifests/openstack/image.pp
+++ profile/manifests/openstack/image.pp
@@ -3,7 +3,8 @@ class profile::openstack::image(
   $notify_enabled   = false,
   $manage_rbd       = false,
   $manage_policy    = false,
-  $manage_notify    = false
+  $manage_notify    = false,
+  $manage_osprofiler = false,
 ) {
 
   include ::profile::openstack::image::api
@@ -24,4 +25,10 @@ class profile::openstack::image(
     include ::glance::notify::rabbitmq
   }
 
+  if $manage_osprofiler {
+    include ::glance::deps
+    $osprofiler_config = lookup('profile::logging::osprofiler::osprofiler_config', Hash, 'first', {})
+    create_resources('glance_api_config', $osprofiler_config)
+    create_resources('glance_registry_config', $osprofiler_config)
+  }
 }
