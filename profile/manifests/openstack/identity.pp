49a15b1686e4b2aa7fe4f7011e790c0cd3028321
Raymond Kristiansen <raymond.kristiansen@uib.no>
2024-05-08 07:38:41 +0200

    feat: upgrade keystone to victoria

--- profile/manifests/openstack/identity.pp
+++ profile/manifests/openstack/identity.pp
@@ -35,6 +35,7 @@ class profile::openstack::identity (
 
   include ::keystone
   include ::keystone::config
+  include ::keystone::cache
   include ::keystone::bootstrap
   include ::keystone::wsgi::apache
   include ::profile::openstack::openrc
