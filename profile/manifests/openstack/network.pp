8fcc6e4737aa9719cae2c66102e54047d3233136
Trond Hasle Amundsen <trondham@uio.no>
2025-01-21 15:51:20 +0100

    torldre test

--- profile/manifests/openstack/network.pp
+++ profile/manifests/openstack/network.pp
@@ -7,6 +7,8 @@ class profile::openstack::network(
   $manage_osprofiler = false,
 ){
   include ::neutron
+  include ::neutron::wsgi::apache
+  include ::neutron::cache
 
 # Use value ml2 for plugin and calico driver for calico v1.3 and older
 # then set neutron::core_plugin to neutron.plugins.ml2.plugin.Ml2Plugin
