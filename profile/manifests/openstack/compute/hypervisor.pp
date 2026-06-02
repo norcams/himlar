3ba85a73ae6f70ac1635e9c5d7c8e92875f00f6c
Trond Hasle Amundsen <trondham@uio.no>
2026-06-02 14:31:28 +0200

    ffoo

--- profile/manifests/openstack/compute/hypervisor.pp
+++ profile/manifests/openstack/compute/hypervisor.pp
@@ -28,31 +28,6 @@ class profile::openstack::compute::hypervisor (
   if $modular_libvirt {
     include ::nova::compute::libvirt::services
     include ::nova::compute::libvirt::virtproxyd
-#    file_line { 'disable auth for virtproxyd tcp socket':
-#      path   => '/etc/libvirt/virtproxyd.conf',
-#      line   => 'auth_tcp = "none"',
-#      match  => '^#auth_tcp = "sasl"',
-#      before => Service['virtproxyd-tcp.socket'],
-#      notify => Service['virtproxyd-tcp.socket']
-#    }
-#    file_line { 'disable tls for virtproxyd tcp socket':
-#      path   => '/etc/libvirt/virtproxyd.conf',
-#      line   => 'listen_tls = 0',
-#      match  => '^#listen_tls = 0',
-#      before => Service['virtproxyd-tcp.socket'],
-#      notify => Service['virtproxyd-tcp.socket']
-#    }
-#    file_line { 'enable tcp for virtproxyd tcp socket':
-#      path   => '/etc/libvirt/virtproxyd.conf',
-#      line   => 'listen_tcp = 1',
-#      match  => '^#listen_tcp = 1',
-#      before => Service['virtproxyd-tcp.socket'],
-#      notify => Service['virtproxyd-tcp.socket']
-#    }
-#    service { 'virtproxyd-tcp.socket':
-#      ensure => running,
-#      enable => true
-#    }
   }
 
   if $hypervisor_type in ['libvirt'] {
